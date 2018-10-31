a2da435916978990a8a073b26f800af2138f90d8
Danni B <danni.ballena@gmail.com>
2018-10-31 09:29:49 -0700

    completed fizzbuzz

--- challenges/fizzbuzz.js
+++ challenges/fizzbuzz.js
@@ -16,8 +16,40 @@
 //                     'fizzbuzz',
 //                     16 ]
 
-function fizzbuzz(num) {
+//input: one single number;
+//output: array of numbers
+//strategy:
+  //create a variable - empty array & a counter (equals to 1)
+  //push counter to array, and increment by 1 until counter is equal to arg;
+  //once numArr is set;
+    //iterate over numArr
+      //if current el is divisible by both 3 and 5; set current el to 'fizzbuzz'
+      //if current el is divisible by 3, set el to 'fizz'
+      //-----------------------------5, set el to 'buzz'
 
+function fizzbuzz(num) {
+  const numArr = [];
+  let counter = 1;
+  while (counter < num+1) {
+    numArr.push(counter);
+    counter++;
+  }
+  //console.log(numArr);
+  for (let i = 0; i < numArr.length; i++){
+    if (numArr[i] % 3 === 0 && numArr[i] % 5 === 0){
+      numArr[i] = 'fizzbuzz'
+    } 
+    if (numArr[i] % 3 === 0){
+      numArr[i] = 'fizz'
+    }
+    if (numArr[i] % 5 === 0){
+      numArr[i] = 'buzz'
+    }
+  }
+
+  return numArr;
 }
+
+console.log(fizzbuzz(16));
 
 module.exports = fizzbuzz;
