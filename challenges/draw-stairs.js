42d00ec107715d1935356ed66f7f1785a3f30c5d
Danni B <danni.ballena@gmail.com>
2018-11-02 10:58:01 -0700

    completed drawStairs

--- challenges/draw-stairs.js
+++ challenges/draw-stairs.js
@@ -12,10 +12,31 @@
 ******
 
 */
+//input: number;
+//output:
+//strategy: 
+    //creating an array with given number of elements;
+    // \n
+    //
 
 function drawStairs(n) {
+    const arr = [];
+    let stairs = '*';
+    let el
+    for (let i = 0; i < n+1; i++){
+            el = stairs.repeat();
+            arr.push(el);
+    
+    }
+    return arr.join('\n');
+//     const arr = Array(n).fill(' ');
+//     let stairs = '*';
+//     for (let i = arr.length; i >= 0; i-1){
+//         arr[i] = stairs
+//         console.log(arr[i]);
+//     }
+// }
 
-}
-
+console.log(drawStairs(10));
 
 module.exports = drawStairs;
