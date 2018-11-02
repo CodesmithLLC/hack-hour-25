d5b848355b6c5e8fc6824a90fac23228f088d947
Jim Yoon <jimyoon90@gmail.com>
2018-11-02 10:04:06 -0700

    didnt finish the stars, printing backwards

--- challenges/draw-stairs.js
+++ challenges/draw-stairs.js
@@ -14,8 +14,19 @@
 */
 
 function drawStairs(n) {
-
+  const star = '*';
+  const spaces = ' ';
+  // start with a for loop that goes from n to 1
+  for (let i = n; i > 0; i -= 1) {
+    // create an empty string for each line
+    let newStr = '';
+    // add by repeating the spaces and star
+    newStr + spaces.repeat(i - 1) + star.repeat(i - n + 1);
+    // after running through all the loops, print the new string and go through the loop again, starting fresh on each line of the stair
+    console.log(newStr);
+  }
 }
+drawStairs(6);
 
 
 module.exports = drawStairs;
