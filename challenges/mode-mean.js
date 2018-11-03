e84404e4e25925130cf5d5ce48b612e54111756e
Jae Hyun Lee <jaehyunlee@Jaes-MacBook-Pro-2.local>
2018-11-03 09:33:16 -0700

    completed mode-mean

--- challenges/mode-mean.js
+++ challenges/mode-mean.js
@@ -9,9 +9,25 @@
  *
  */
 
-
 function modemean(array) {
-
+	let total = 0;
+	const frequency = {};
+	const modeArray = [];
+	for (let index in array) {
+		total += array[index];
+		if (frequency[array[index]]) frequency[array[index]] += 1;
+		else frequency[array[index]] = 1;
+	}
+	const mean = Math.floor(total / array.length);
+	const modeVal = Math.max(...Object.values(frequency));
+	for(let keys in frequency) {
+		if (frequency[keys] === modeVal) modeArray.push(keys);
+	}
+	if (modeArray.length === 1) return modeArray[0] == mean;
+	else {
+		let mode = Math.max(...modeArray);
+		return mode == mean;
+	}
 }
 
 module.exports = modemean;
