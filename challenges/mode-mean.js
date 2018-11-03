83958863d3b3f5fd7f96eda268beaa65fca93620
Tony Hudgins <tonyhudgins@gmail.com>
2018-11-03 10:38:38 -0700

    completed mode-mean challenge

--- challenges/mode-mean.js
+++ challenges/mode-mean.js
@@ -11,7 +11,22 @@
 
 
 function modemean(array) {
+    const mean = array.reduce((acc, c, i, a) => acc + c/a.length ,0)
 
+    const counts = array.reduce((acc, c, i, a) => {
+        acc[c] = (acc[c]||0) + 1
+        return acc
+    } , {} )
+
+    let max = null;
+    for (let node in counts) {
+        if (max === null) max = node
+        if (counts[node] > counts[max] ) max = node
+    }
+
+    const mode = Number(max)
+
+    return Math.floor(mean) === mode
 }
 
 module.exports = modemean;
