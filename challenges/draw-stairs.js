047956e88314b01860e70b24d30730c9679e878f
codemccodeface <yurenfrankma@gmail.com>
2018-11-03 09:33:20 -0700

    finished modemean

--- challenges/draw-stairs.js
+++ challenges/draw-stairs.js
@@ -14,8 +14,23 @@
 */
 
 function drawStairs(n) {
-
+  // use a for loop to create a stirng of current space and n - current asterix
+  let current = 0;
+  while (current <= n) {
+    let result = '';
+    for (let i = 0; i < n; i += 1) {
+      if (i < n - current) {
+        result += ' ';
+      } else {
+        result += '*';
+      }
+    }
+    console.log(result);
+    current += 1;
+  }
+  // if (current <= n) drawStairs(n, current);
+  // recurse drawStairs current + 1 unti current = n
 }
 
-
+drawStairs(5);
 module.exports = drawStairs;
--- challenges/mode-mean.js
+++ challenges/mode-mean.js
@@ -11,7 +11,31 @@
 
 
 function modemean(array) {
-
+  const total = array.reduce((a, b) => a + b);
+  const mean = Math.floor(total / array.length);
+  const storage = {};
+  // iterate through the array, get save as key value pair to obj.  key = numer and val = amt of times called.  
+  for (let i = 0; i < array.length; i += 1) {
+    if (storage[array[i]]) {
+      storage[array[i]] += 1;
+    } else {
+      storage[array[i]] = 1;
+    }
+  }
+  //find the max(s) of the values 
+  const numCounts = Object.values(storage);
+  const modeCount = Math.max(...numCounts);
+  // get the associated keys, turn from string to num, and find the max
+  const modeArray = [];
+  const numCountPairs = Object.entries(storage);
+  for (let i = 0; i < numCountPairs.length; i += 1) {
+    const [num, count] = numCountPairs[i];
+    if (count === modeCount) modeArray.push(num);
+  }
+  const maxMode = Math.max(...modeArray);
+  // compare if equal
+  return mean === maxMode;
 }
 
+console.log(modemean([1, 2, 3, 4, 3, 4]));
 module.exports = modemean;
