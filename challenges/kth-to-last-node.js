6ca52626ab0de9e430e7a12fbb113ce69b0de0d8
vwang4536 <vwang4536@gmail.com>
2018-11-06 10:44:05 -0800

    kth to last node completed

--- challenges/kth-to-last-node.js
+++ challenges/kth-to-last-node.js
@@ -1,20 +1,19 @@
-/**
- * Write a function that takes two parameters, an integer and the head of a
- * singly linked list, and returns the VALUE on the kth to last node in the list.
- *
- * const a = new Node('A');
- * const b = new Node('B');
- * const c = new Node('C');
- * const d = new Node('D');
- * const e = new Node('E');
- *
- * a.next = b;
- * b.next = c;
- * c.next = d;
- * d.next = e;
- *
- * kthToLastNode(2, a); -> returns 'D' (the value on the second to last node)
- */
+
+// * Write a function that takes two parameters, an integer and the head of a
+ //* singly linked list, and returns the VALUE on the kth to last node in the list.
+// *
+ const a = new Node('A');
+ const b = new Node('B');
+ const c = new Node('C');
+ const d = new Node('D');
+ const e = new Node('E');
+ 
+ a.next = b;
+ b.next = c;
+ c.next = d;
+ d.next = e;
+// *
+ 
 
 function Node(val) {
   this.value = val;
@@ -22,7 +21,20 @@ function Node(val) {
 }
 
 function kthToLastNode(k, head) {
-
+  let count = 0;
+  let length = 0;
+  let current = head;
+  let current1 = head;
+  while (current !== null) {
+    length += 1;
+    current = current.next
+  }
+  while (length - count !== k) {
+//     console.log(current1)
+    current1 = current1.next;
+    count += 1
+  }
+  return current1.value
 }
-
+console.log(kthToLastNode(2, a));// -> returns 'D' (the value on the second to last node)
 module.exports = {Node: Node, kthToLastNode: kthToLastNode};
