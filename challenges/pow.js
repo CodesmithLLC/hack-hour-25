0a20d190a73ed442de9e55c09d7daa63cf8c171b
Darryl Amour <darryl.amour@gmail.com>
2018-11-07 09:42:15 -0800

    Completed and tested pow.js

--- challenges/pow.js
+++ challenges/pow.js
@@ -2,7 +2,19 @@
  * Use recursion!
  */
 
-function pow(base, power) {
+function pow(base, power, result = 1) {
+
+
+  // Base Case: when the power is equal to zero exit recursion
+  if (power === 0) return result;
+
+  // Edge case if base is zero return 0
+  if (base === 0) return 0;
+
+  // Edge case if power is zero return 1
+  if (power === 0) return 1;
+
+  return pow(base, (power -1), base * result);
 
 }
 
