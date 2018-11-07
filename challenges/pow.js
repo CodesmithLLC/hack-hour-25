7d78979c9bdd12c7bc7c986de61dd941622c128b
Tony Hudgins <tonyhudgins@gmail.com>
2018-11-07 09:51:54 -0800

    solves pow for positive, fractional and negative numbers

--- challenges/pow.js
+++ challenges/pow.js
@@ -3,7 +3,10 @@
  */
 
 function pow(base, power) {
-
+    if (power === 1) return base
+    if (power === 0) return 1
+    if (power < 0)  return Math.exp(power*Math.log(base));
+    return base * pow(base, power-1)
 }
 
 module.exports = pow;
