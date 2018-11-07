5c452bde6ece85f941218e0a3cc3349913eda31e
Jim Yoon <jimyoon90@gmail.com>
2018-11-07 09:39:01 -0800

    completed pow.js for hack hour

--- challenges/pow.js
+++ challenges/pow.js
@@ -3,7 +3,24 @@
  */
 
 function pow(base, power) {
-
+  // base case
+  // if power is 0, then just return 1
+  if (power === 0) {
+    return 1;
+  }
+  // if power is 1, then just return the base
+  if (power === 1) {
+    return base;
+  }
+  // recursive function
+  return base * pow(base, power - 1);
 }
+
+// console.log(pow(5, 2)); // 25
+// console.log(pow(3, 2)); // 9
+// console.log(pow(3, 3)); // 27
+// console.log(pow(6, 2)); // 36
+// console.log(pow(6, 0)); // 1
+
 
 module.exports = pow;
