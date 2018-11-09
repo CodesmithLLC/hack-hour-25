a3350f05302914d63af5c50c0ce9a91422d16e77
Dave Marquess <dave.marquess@gmail.com>
2018-11-09 10:01:50 -0800

    attempt reverse-in-place challenge

--- challenges/reverse-in-place.js
+++ challenges/reverse-in-place.js
@@ -8,13 +8,26 @@
  * such as Python and Javascript, strings are immutable, meaning they cannot
  * be changed after they're created.
  *
- * Since strings are immutable in javascript, we will be reversing an array of characters instead.
+ * Since strings are immutable in javascript, we will be reversing an array of characters
+ * instead.
  *
  * DO NOT USE THE BUILT IN REVERSE METHOD
  */
 
 function reverseInPlace(array) {
-
+  let sub = 0;
+  let half = Math.ceil(array.length / 2);
+  // iterate thru array
+  for (let i = array.length - 1; i > half; i -= 1) {
+    // for each char in array,
+    if (array.length % 2 === 0 && sub < Math.floor(array.length / 2) - 1) {
+      let temp = array[i];
+      array[i] = array[sub];
+      array[sub] = temp;
+      sub += 1;
+    }
+  }
+  return array;
 }
 
 module.exports = reverseInPlace;
