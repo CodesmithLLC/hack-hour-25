5309a7e0de77a2321219c3f0ebef8d23cbba2416
Hei Yeung Lam <heiyeunglam@Heis-MacBook-Pro.local>
2018-11-10 09:30:36 -0800

    string-rotation

--- challenges/string-rotation.js
+++ challenges/string-rotation.js
@@ -11,12 +11,25 @@
  *              stringRotation("hello", "ollhe") -> false (not a rotation, just an anagram)
  */
 
-function isSubstring(s1, s2) {
-  return s1.indexOf(s2) >= 0;
-}
 
 function stringRotation(s1, s2) {
-
+  //edge case: check if both strings are of type string
+  if (typeof s1 !== "string" || typeof s2 !== "string") return undefined
+  //if the s2 is less than s1, s2 can't be a rotation of s1
+  if (s2.length < s1.length) return false
+  let i = 1
+  // set up a pointer to slice the s2 string at index 1 to length - 1;
+  // store slice string in substring1 and substring2
+  //set up a loop where we increment the pointer,
+  while (i < s1.length) {
+    let substring1 = s2.slice(0,i);
+    let substring2 = s2.slice(i,s2.length);
+    //return true if both substring are substring of s1 as well
+    if (isSubstring(s1,substring1) && isSubstring(s1,substring2)) return true
+    i += 1;
+  }
+  //return false if no rotation was found
+  return false
 }
 
 module.exports = {isSubstring: isSubstring, stringRotation: stringRotation};
