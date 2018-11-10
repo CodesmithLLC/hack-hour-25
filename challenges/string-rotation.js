cbaa15f3d89b4e919ac052e622009a93a7f01078
Timothy Kachler <kachler@mac.com>
2018-11-10 09:59:02 -0800

    completed string-rotation challenge

--- challenges/string-rotation.js
+++ challenges/string-rotation.js
@@ -16,7 +16,35 @@ function isSubstring(s1, s2) {
 }
 
 function stringRotation(s1, s2) {
+  // If string lengths don't match, return false
+  if (s1.length !== s2.length) return false;
 
+  // Split strings into arrays
+  const array1 = s1.split('');
+  const array2 = s2.split('');
+
+  // Grab index of last array element
+  const last = array2.length - 1;
+
+  let len = array1.length;
+
+  // Iternate through arrays until first characters match
+  // or the loop have run array length number of times
+  for (let i = 0; i < array1.length; i++) {
+    if (array1[0] !== array2[0]) {
+      const temp = array2[last];
+      array2.pop();
+      array2.unshift(temp);
+      len -= 1;
+    }
+  }
+  return isSubstring(array1.join(''), array2.join(''));
 }
+
+console.log(stringRotation('hello', 'hello')); // returns true
+console.log(stringRotation('hello', 'llohe')); // returns true
+console.log(stringRotation('hello', 'ollhe')); // returns false
+console.log(stringRotation('hello', 'he')); // returns false
+console.log(stringRotation('hello', 'apple')); // returns false
 
 module.exports = {isSubstring: isSubstring, stringRotation: stringRotation};
