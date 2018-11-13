755e1245c7e342cf142f26e1cebfe5e55ff38a7b
Darryl Amour <darryl.amour@gmail.com>
2018-11-13 10:03:08 -0800

    Completed balanced-parens.js only for regular parens, was not able to enhance logic to handle different types

--- challenges/balanced-parens.js
+++ challenges/balanced-parens.js
@@ -24,8 +24,35 @@
  *
  */
 
-function balancedParens(input){
-
+function balancedParens(input) {
+  let resultsBool = false; // declare boolean results variable intialize to false
+  // tracker for number of open parens
+  let numOpenParens = 0; 
+  let inputAsArray = input.split('');
+  // Iterate thru input string
+  //console.log(`before start resultsBool-numOpenParens-inputAsArray: ${resultsBool}-${numOpenParens}-["${inputAsArray}"]`);
+  resultsBool = inputAsArray.reduce((accum, val, indx, arr) =>{
+    // Edge Case: if first parens is a closing parens exit with false;
+    //console.log(`Inside reduce accum-numOpenParens-val: ${accum}-${numOpenParens}-["${val}"]`);
+    if (val === ')' && numOpenParens === 0) {
+      accum = false;
+      return accum;   
+    }
+    if (val === ')' && numOpenParens > 0) {
+      numOpenParens -= 1;  // Decrease Open parens count
+    }
+    if (val === '(' ) {
+      numOpenParens += 1;  // Increase Open parens count
+    }
+    // Edge Case: Open parens and end of string
+    if ((arr.length - 1) === indx && numOpenParens > 0) {
+      accum = false;
+      return accum;
+    }
+    return  accum;
+  }, true);
+  // return boolean results variable
+  return resultsBool;
 }
 
 module.exports = balancedParens;
