d4b6cb34caf6cb2aa14929ed60eba2c81fa57e30
Hei Yeung Lam <heiyeunglam@Heis-MacBook-Pro.local>
2018-11-13 09:28:54 -0800

    balance-parens

--- challenges/balanced-parens.js
+++ challenges/balanced-parens.js
@@ -25,7 +25,30 @@
  */
 
 function balancedParens(input){
+	//edge case: return undefined if not string
+	if (typeof input !== "string") return undefined;
 
+	//for step3, ignore non bracket characters
+	input = input.replace(/[^(){}\[\]]/g,"");
+	// brackets will always be unbalanced if it's not divisible by 2
+  	if ((input.length % 2) !== 0) return false;
+  	//create a stack to keep track of brackets as we traverse through the string
+	let stack = [];
+	//put the other side of brackets in the stack depending on the type of brackets
+	for (let index in input) {
+		if (input[index] === "(" || input[index] === "[" || input[index] === "{" ) {
+      if (input[index] === "(") stack.push(")");
+      if (input[index] === "[") stack.push("]");
+      if (input[index] === "{") stack.push("}");
+
+     //remove the bracket from stack if we found a matching bracket
+		} else if (stack[stack.length - 1] === input[index]) {
+			stack.pop();
+		}
+	}
+
+
+	return (stack.length === 0)? true : false;
 }
 
 module.exports = balancedParens;
