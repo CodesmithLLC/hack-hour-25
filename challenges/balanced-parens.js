023083b2edf12f89ebc07ebc8709610f6480df1c
evgenii-codesmith <evgenii.codesmith@gmail.com>
2018-11-13 10:22:56 -0800

    balanced parens.Need to change my alg.Not meeting all test cases.

--- challenges/balanced-parens.js
+++ challenges/balanced-parens.js
@@ -25,7 +25,44 @@
  */
 
 function balancedParens(input){
+	let obj2compare = {'(': ')',
+	'{':'}', 
+	'[':']'
+	}
 
-}
+	let input2array = input.split('');
+	for (key in obj2compare){
+		if (input2array.indexOf(key) !== -1){
+			let index = input2array.indexOf(key);
+			if (obj2compare[key] === input2array[index+1]) return true;
 
+		}
+		
+	}
+	
+	return false;
+		
+	}
+	
+
+
+//let obj2compare = {'(': ')', '{':'}', '[':']'}
+
+// console.log (obj2compare['('])
+
+// console.log(Object.keys(obj2compare).indexOf('c'));
+
+//console.log('abcde'.split(''))
+
+//console.log(balancedParens('abcde'))
+// console.log(balancedParens('[](){}')); // true
+// console.log(balancedParens('[({})]'));   // true
+// console.log(balancedParens('[(]{)}')); // false
+
+
+ console.log(balancedParens('('));  // false
+ console.log(balancedParens('()')); // true
+ console.log(balancedParens(')('));  // false
+ console.log(balancedParens('(())'));  // true
+  
 module.exports = balancedParens;
