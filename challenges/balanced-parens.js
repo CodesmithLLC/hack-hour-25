eea354b57bf91e3d241c40f0eb493292d8f56a6e
codemccodeface <yurenfrankma@gmail.com>
2018-11-13 10:00:15 -0800

    finished balance parens

--- challenges/balanced-parens.js
+++ challenges/balanced-parens.js
@@ -24,8 +24,46 @@
  *
  */
 
-function balancedParens(input){
+const left = ['[', '(', '{'];
+const right = [']', ')', '}'];
 
+function isMirror(string) {
+  if (string.length < 2) return false;
+  const halfway = Math.floor(string.length / 2);
+  for (let i = 0; i < halfway; i += 1) {
+    if (left.indexOf(string[i]) < 0) return false;
+    if (left.indexOf(string[i]) !== right.indexOf(string[string.length - 1 - i])) return false;
+  }
+  return true;
 }
+
+
+function isFlip(string) {
+  if (string.length < 2) return false;
+  for (let i = 0; i < string.length - 1; i += 2) {
+    if (left.indexOf(string[i] < 0)) return false;
+    if (left.indexOf(string[i]) !== right.indexOf(string[i + 1])) return false;
+  }
+  return true;
+}
+
+function balancedParens(input) {
+  const newInput = input.replace(/[^\[\(\{\}\)\]]/g, '');
+  // console.log(newInput)
+  // console.log(isMirror(newInput))
+  return isFlip(newInput) || isMirror(newInput);
+}
+
+// console.log(balancedParens('{()}'))
+// console.log(balancedParens('()')); 
+console.log(balancedParens(')('));  // false
+// console.log(balancedParens('(())'));  // true
+// console.log(balancedParens('[](){}'));
+
+console.log(balancedParens(' var wow  = { yo: thisIsAwesome() }'))
+console.log(balancedParens('()')); 
+console.log(balancedParens(')('));  // false
+console.log(balancedParens(' var hubble = function() { telescopes.awesome();'));  // false
+
 
 module.exports = balancedParens;
