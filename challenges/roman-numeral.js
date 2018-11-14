c70a81eef8c386837bc870f24edb9ff55a79358e
Hei Yeung Lam <heiyeunglam@Heis-MacBook-Pro.local>
2018-11-14 09:43:41 -0800

    roman-numeral

--- challenges/roman-numeral.js
+++ challenges/roman-numeral.js
@@ -18,7 +18,45 @@
  */
 
 function romanNumeral(n) {
+  //edge case: result undefined if not a number;
+  if (typeof n !== "number") return undefined;
 
+	let table = {
+		1000: "M",
+		900: "CM",
+		500: "D",
+		400: "CD",
+		100: "C",
+		90: "XC",
+		50: "L",
+		40: "XL",
+		10: "X",
+		9: "IX",
+		5: "V",
+		4: "IV",
+		1: "I",
+	}
+
+  // create an array of table's key as integer;
+	let arr = Object.keys(table)
+  arr = arr.map(el => Number(el)).reverse()
+  // create a while loop to keep subtracting from the n as we travese through the array;
+  let i = 0;
+  //declare a variable to store the string.
+  let result = "";
+	while (n > 0) {
+    //if n is bigger than a number in the element, we substract that number until the n is smaller than the element, then we move to the next one;
+    if (n >= arr[i]) {
+      do {
+      result += table[arr[i]]
+      console.log(n)
+      n = n - arr[i];
+      } while (n >= arr[i])
+    }
+  i ++;
+	}
+
+  return result;
 }
 
 module.exports = romanNumeral;
