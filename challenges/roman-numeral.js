0dffcea9f5dbcddc3d68cc9e322231715d46c0b4
vwang4536 <vwang4536@gmail.com>
2018-11-14 10:02:03 -0800

    completed roman numerals

--- challenges/roman-numeral.js
+++ challenges/roman-numeral.js
@@ -18,7 +18,22 @@
  */
 
 function romanNumeral(n) {
-
+  // create empty string
+  let str = '';
+  // create arrays for numbers and roman numeral
+  const rm = ['M', 'CM', 'D', 'CD', 'C', 'XC', 'L', 'XL', 'X', 'IX', 'V', 'IV', 'I'];
+  const numArr = [ 1000, 900, 500, 400, 100, 90, 50, 40, 10, 9, 5, 4, 1 ];
+  // iterate up to the array length
+  // create a while loop to check if n is currently greater than each numArr
+  // if n is greater then push the roman numeral into the str and subtract n
+  for (let i = 0; i < numArr.length; i += 1) {
+    while (n >= numArr[i]) {
+      str += rm[i];
+      n -= numArr[i];
+    }
+  }
+  // return the string
+  return str;
 }
-
-module.exports = romanNumeral;
+console.log(romanNumeral(2018));
+// module.exports = romanNumeral;
