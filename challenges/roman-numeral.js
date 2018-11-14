0a2c8b24670d47cafa2432b187ba8e595f3dead6
Darryl Amour <darryl.amour@gmail.com>
2018-11-14 10:03:18 -0800

    Completed roman-numeral.js not confident on all test cases

--- challenges/roman-numeral.js
+++ challenges/roman-numeral.js
@@ -17,8 +17,69 @@
  * 
  */
 
-function romanNumeral(n) {
+function romanNumeral(n, romanNumeralStr='', remainder=n) {
+  // Base Case: n must be positive (> 0)
+  if (remainder < 0) return 'Please provide a positive integer';
+  if (remainder === 0) return romanNumeralStr;
 
+  // Process from largest to smallest
+  switch (true) {
+    case (remainder >= 1000):  // M
+      romanNumeralStr += 'M';
+      remainder -= 1000;
+      break;
+    case (remainder >= 900):
+      romanNumeralStr += 'CM';
+      remainder -= 900;
+      break;
+    case (remainder >= 500):
+      romanNumeralStr += 'D';
+      remainder -= 500;
+      break;
+    case (remainder >= 400):
+      romanNumeralStr += 'CD';
+      remainder -= 400;
+      break;
+    case (remainder >= 100):
+      romanNumeralStr += 'C';
+      remainder -= 100;
+      break;
+    case (remainder >= 90):
+      romanNumeralStr += 'XC';
+      remainder -= 90;
+      break;
+    case (remainder >= 50):
+      romanNumeralStr += 'L';
+      remainder -= 50;
+      break;
+    case (remainder >= 40):
+      romanNumeralStr += 'XL';
+      remainder -= 40;
+      break;
+    case (remainder >= 10):
+      romanNumeralStr += 'X';
+      remainder -= 10;
+      break;
+    case (remainder >= 9):
+      romanNumeralStr += 'IX';
+      remainder -= 9;
+      break;
+    case (remainder >= 5):
+      romanNumeralStr += 'V';
+      remainder -= 5;
+      break;
+    case (remainder >= 4):
+      romanNumeralStr += 'IV';
+      remainder -= 4;
+      break;
+    case (remainder >= 1):
+      romanNumeralStr += 'I';
+      remainder -= 1;
+      break;
+  }
+
+  // Recursively Call
+  return romanNumeral(remainder, romanNumeralStr, remainder);
 }
 
 module.exports = romanNumeral;
