73bd9065bf4c926fcd15977486f9efac63a506e7
Dave Marquess <dave.marquess@gmail.com>
2018-11-16 10:00:53 -0800

    attempt subset-sum challenge

--- challenges/subset-sum.js
+++ challenges/subset-sum.js
@@ -1,5 +1,6 @@
 /* You are given an array of integers and a target number. Write a function that returns true if
- * there is a subset of the array that sums up to the target and returns false otherwise. A subset
+ * there is a subset of the array that sums up to the target and returns false otherwise. 
+ A subset
  * can be any size and the elements do not have to appear consecutively in the array.
  * 
  * subsetSum([3, 7, 4, 2], 5) - > true, 3 + 2 = 5
@@ -8,8 +9,28 @@
  * subsetSum([8, -2, 1, -3], 6) -> true, 8 + 1 + (-3) = 6
  */
 
-function subsetSum(array, target) {
+//inputs: array of nums, target num
+//output: boolean
 
+function subsetSum(array, target, sum = 0) {
+  // sort array
+  const arrSorted = array.sort((a, b) => a - b);
+  // base case
+  if (sum === target) return true;
+  // iterate thru array, checking if current val plus sum is greater than array.
+  for (let i = 0; i < arrSorted.length; i += 1) {
+    if (sum === target) return true;
+    if (sum > target) {
+      sum -= arrSorted[i - 1];
+      return subsetSum(arrSorted, target, sum);
+    }
+    sum += arrSorted[i];
+  }
 }
 
 module.exports = subsetSum;
+
+// console.log(subsetSum([3, 7, 4, 2], 5));
+// console.log(subsetSum([3, 34, 4, 12, 5, 12], 32))
+// console.log(subsetSum([8, 2, 4, 12], 13))
+// console.log(subsetSum([8, -2, 1, -3], 6))
