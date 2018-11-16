17d827afef8c5774a0fc73ee5c49a7bea28f9d61
Timothy Kachler <kachler@mac.com>
2018-11-16 10:03:06 -0800

    completed subset-sum challenge

--- challenges/subset-sum.js
+++ challenges/subset-sum.js
@@ -9,7 +9,29 @@
  */
 
 function subsetSum(array, target) {
+    
+  // Iterate through array storing the first index to a total variable
+  for (let i = 0; i < array.length; i += 1) {
+    // Declare a total variable
+    let total = array[0];
 
+    // Iterate through array starting at index 1 and adding each element to the total
+    for (let j = 1; j < array.length; j += 1) {
+      total += array[j];
+      
+      // Check of the total matches the target
+      if (total === target) return true;
+
+      // Shift the first element of the array and push it to the end
+      array.push(array.shift());
+    }
+  }
+  return false;
 }
+
+console.log(subsetSum([3, 7, 4, 2], 5)); // - > true, 3 + 2 = 5
+console.log(subsetSum([3, 34, 4, 12, 5, 12], 32)); // -> true, 3 + 12 + 5 + 12 = 32
+console.log(subsetSum([8, 2, 4, 12], 13)); // -> false
+console.log(subsetSum([8, -2, 1, -3], 6)); // -> true, 8 + 1 + (-3) = 6
 
 module.exports = subsetSum;
