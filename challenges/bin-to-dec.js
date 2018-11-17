85c99df9ed14443e13bcab6bc56f3b49ac5c57cd
Darryl Amour <darryl.amour@gmail.com>
2018-11-17 10:56:38 -0800

    Completed bin-to-dec.js, still debugging extension decToBin()

--- challenges/bin-to-dec.js
+++ challenges/bin-to-dec.js
@@ -13,8 +13,31 @@
  * Write a function that converts a decimal number to binary (then maybe hexadecimal)
  */
 
-function binToDec(binary) {
+function binToDec(binary, index = binary.length - 1, exponent = 0, result = 0) {
 
+  // Base Case return when index less than zero
+  if (index < 0) return result;
+  // Calculate traversing binary string from right to left increasing exponent by 1
+  // Binary is base 2, decimal is base 10
+  if (binary[index] === '1') {
+    result += (2 ** exponent);
+  }
+  return binToDec(binary, index - 1, exponent + 1, result);
 }
 
-module.exports = binToDec;
+function decToBin(decimal, remainder = decimal, exponent, result = 0) {
+  // console.log(exponent);
+  // console.log(result);
+  exponent = Math.floor(Math.sqrt(remainder));
+  // Base Case return when remainder equals zero
+  if (remainder === 0) return result;
+  // Calculate by determining highest value also the remainder
+  if ((2 ** exponent) >= remainder) {
+    remainder = (2 ** exponent) - remainder; 
+    result += Number('1' + '0'.repeat(exponent - 1));
+  }
+  console.log(remainder);
+  return decToBin(decimal, remainder, exponent - 1, result);
+}
+
+module.exports = {binToDec:binToDec, decToBin:decToBin};
