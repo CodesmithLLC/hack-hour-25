74f59508802549ddd0984fa6cb72cbadd0d0c1de
Hei Yeung Lam <heiyeunglam@Heis-MacBook-Pro.local>
2018-11-17 14:47:48 -0800

    challenges/bin-to-dec.js

--- challenges/bin-to-dec.js
+++ challenges/bin-to-dec.js
@@ -14,7 +14,30 @@
  */
 
 function binToDec(binary) {
-
+  let sum = 0;
+  for (let i = 0, j = binary.length - i; i < binary.length; i++, j--) {
+    if (binary[i] === '1') {sum += Math.pow(2, j - 1 )}
+  }
+  return sum
 }
 
+function decToBin(dec) {
+  //declare a variable result that store the result as binary
+  let result = 0;
+ //do a while loop to count the number of 2 power right before reaching above the dec,
+  let n = 0;
+  while (Math.pow(2,n) <= dec) {
+   n += 1;
+  }
+  let remain = dec - Math.pow(2, n - 1)
+  result = Math.pow(10, n - 1)
+  for (let i = 0, j = n - 1; i <= n -1; i ++, j--){
+    if (Math.pow(2,j - 1) <= remain) {
+      result += Math.pow(10, j-1)
+      remain -= Math.pow(2,j-1 )
+    }
+  }
+  return result.toString()
+}
+//'3' -> 11
 module.exports = binToDec;
