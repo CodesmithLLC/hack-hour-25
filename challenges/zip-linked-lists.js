9a04c0c5464de5a710483db3e00e83e8379fe369
Hei Yeung Lam <heiyeunglam@Heis-MacBook-Pro.local>
2018-11-21 09:56:28 -0800

    zip

--- challenges/zip-linked-lists.js
+++ challenges/zip-linked-lists.js
@@ -10,7 +10,24 @@ function Node(val) {
   this.next = null;
 }
 
-function zip(l1, l2) {
+function zip(l1, l2, pointCounter = 0) {
+	//declare variable temp to store the nocde that l1's head points to
+  if (l2 === null) return l1
+	let temp;
+	//declare pointCounter  === 1
+	let position = l1.next;
+  let listPointer = l1;
+	//get to the node whose next you can to change
+	// put l1's next in temp
+	for (let i = 0; i < pointCounter ; i ++) {
+    if (pointCounter > 0) {listPointer = listPointer.next}
+		position = position.next;
+	}
+  temp = position
+	//have l1's next points to l2
+	pointCounter ++;
+	listPointer.next = l2;
+	return zip(l1, temp, pointCounter)
 };
 
 module.exports = {Node: Node, zip: zip};
