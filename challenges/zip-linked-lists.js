577a199f5c53d0a476f7758580010a696597352e
Jason Wong <jasonwong@Jasons-MacBook-Pro.local>
2018-11-21 09:57:07 -0800

    zip first attempt

--- challenges/zip-linked-lists.js
+++ challenges/zip-linked-lists.js
@@ -11,6 +11,26 @@ function Node(val) {
 }
 
 function zip(l1, l2) {
+  let current1 = l1;
+  let current2 = l2;
+  let holder1;
+  let holder2;
+  while (current1.next !== null && current2.next !== null) {
+    holder1 = current1.next;
+    holder2 = current2.next;
+    current1.next = current2;
+    current2.next = holder1;
+    current1 = holder1;
+    current2 = holder2;
+  }
+  if (current1.next === null) {
+    current1.next = current2;
+  }
+  else if (current2.next === null) {
+    holder1 = current1.next;
+    current1.next = current2;
+    current2.next = holder1;
+  }
 };
 
 module.exports = {Node: Node, zip: zip};
