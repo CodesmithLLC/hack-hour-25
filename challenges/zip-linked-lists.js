9ae8d78b20d59cb6a4f48879e2355bcaf85e7f8a
Darryl Amour <darryl.amour@gmail.com>
2018-11-21 10:15:41 -0800

    Completed zip-linked-lists.js.  Not properly tested, logic has been dry run several times.

--- challenges/zip-linked-lists.js
+++ challenges/zip-linked-lists.js
@@ -11,6 +11,41 @@ function Node(val) {
 }
 
 function zip(l1, l2) {
+
+  // define pointers for next nodes and current
+  let currentNode;
+  let l1NextPointer;
+  let l2NextPointer;
+  let l1Done = false;
+  let l2Done = false;
+
+  if (l1 === null || l2 === null) {
+    return;
+  }
+
+  currentNode = l1;
+  l1NextPointer = l1.next;
+  l2NextPointer = l2;
+
+  while (!l1Done && !l2Done) {
+    // Build List
+    currentNode.next = l2NextPointer;
+    currentNode = l2NextPointer;
+
+    // Advance L2
+    if (l2NextPointer.next !== null) {
+      l2NextPointer = l2NextPointer.next;
+    } else l2Done = true;
+    currentNode.next = l1NextPointer;
+    // Advance L1
+    if (l1NextPointer.next !== null) {
+      l1NextPointer = l1NextPointer.next;
+    } else l1Done = true;
+    currentNode = currentNode.next;
+  }
+
+  // return first node of new zipped linked list
+  return l1;
 };
 
 module.exports = {Node: Node, zip: zip};
