204e47f4a81160533b2f3871419523ccea959e83
Adrian <34041016+Sgshi@users.noreply.github.com>
2018-11-21 09:49:14 -0800

    complete zip linked link lists

--- challenges/zip-linked-lists.js
+++ challenges/zip-linked-lists.js
@@ -11,6 +11,37 @@ function Node(val) {
 }
 
 function zip(l1, l2) {
-};
+  let link1 = l1;
+  const output = link1;
+  let link2 = l2;
 
-module.exports = {Node: Node, zip: zip};
+  while (link1) {
+    //value to hold the list in place
+    let l1hold;
+    let l2hold = link2;
+
+    //set placeholder equal to the next from the current node
+    l1hold = link1.next;
+
+    link1.next = link2;
+
+    l2hold = link2.next;
+
+    link1.next.next = l1hold;
+
+    link1 = link1.next.next;
+    link2 = l2hold;
+
+    if (!link1 && link2) {
+      link1 = link2;
+      break;
+    }
+    if (link1 && !link2) {
+      break;
+    }
+  }
+  return output;
+}
+
+
+module.exports = { Node: Node, zip: zip };
