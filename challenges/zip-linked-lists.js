9fcc3bdff691c0c770a4d88f5641e7913a71d45b
vwang4536 <vwang4536@gmail.com>
2018-11-21 09:56:54 -0800

    zip-linked-list testing

--- challenges/zip-linked-lists.js
+++ challenges/zip-linked-lists.js
@@ -11,6 +11,18 @@ function Node(val) {
 }
 
 function zip(l1, l2) {
+  // create the head from the first node in l1
+  const current1 = l1[0];
+  const current2 = l2[0];
+  while (current1 !== null) {
+    const next2 = current2.next
+    const next1 = current1.next;
+    current1.next = current2;
+    if (next1 === null) {break};
+    current2.next = next1;
+    current1 = current2.next;
+    current2 = next2;
+  }
 };
 
 module.exports = {Node: Node, zip: zip};
