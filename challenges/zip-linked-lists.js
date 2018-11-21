21d5da825a3ed60889032c4962e59e87a5449426
Jae Hyun Lee <jaehyunlee@Jaes-MacBook-Pro-2.local>
2018-11-21 09:52:21 -0800

    zip linked list

--- challenges/zip-linked-lists.js
+++ challenges/zip-linked-lists.js
@@ -11,6 +11,28 @@ function Node(val) {
 }
 
 function zip(l1, l2) {
+  
+  // in the case either l1 or l2 is undefined, return the other
+  if (l1 === undefined) return l2;
+  if (l2 === undefined) return l1;
+  
+  // initialize variables to start at l1 and l2, respectively
+  let l1Curr, l1Tracker = l1;
+  let l2Curr, l2Tracker = l2;
+
+  // only iterate through when both l1Curr and l2Curr are defined
+  while (l1Curr !== undefined && l2Curr !== undefined) {
+    // set l1Tracker to hold the next node of l1Curr
+    l1Tracker = l1Curr.next();
+    l1Curr.next = l2Curr;
+    // set l2Tracker to hold the next node of l2Curr
+    l2Tracker = l2Curr.next();
+    l2Curr.next = l1Tracker;
+    // set l1Curr and l2Curr to where the trackers are
+    l1Curr = l1Tracker;
+    l2Curr = l2Tracker;
+  }
+  return l1;
 };
 
 module.exports = {Node: Node, zip: zip};
