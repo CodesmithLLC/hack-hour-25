5ef14089e78cb3d2272d4e6f51f0c3966c83c70e
Jason Wong <jasonwong@Jasons-MacBook-Pro.local>
2018-11-27 09:59:28 -0800

    reverse

--- challenges/reverse-linked-list.js
+++ challenges/reverse-linked-list.js
@@ -14,7 +14,22 @@ function Node(value) {
 }
 
 function reverseLinkedList(head) {
-
+  if (!head) return;
+  let c1 = head;
+  let c2 = head.next;
+  if (c2 === null) {
+    return c1;
+  }
+  let holder = c2.next;
+  c1.next = null;
+  c2.next = c1;
+  while (holder !== null) {
+    c1 = c2;
+    c2 = holder;
+    holder = holder.next;
+    c2.next = c1;
+  }
+  return c2;
 }
 
 module.exports = {Node: Node, reverseLinkedList: reverseLinkedList};
