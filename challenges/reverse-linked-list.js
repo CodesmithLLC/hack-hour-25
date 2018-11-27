69d5ddeafcd4a33d6bed475c102b3df34161c859
Darryl Amour <darryl.amour@gmail.com>
2018-11-27 10:56:34 -0800

    internal testing was not commented out. Clean up code

--- challenges/reverse-linked-list.js
+++ challenges/reverse-linked-list.js
@@ -22,11 +22,13 @@ function reverseLinkedList(head, oldParentNode = head, curNode, tail = head) {
   // Edge Case: when head is null
   if (head == null) return head;
 
+  // assign only after edge case for null head passed
   curNode = tail.next;
 
   // Base Case: when curNode is null end of list
   if (curNode == null) return oldParentNode;
 
+  // build new list
   tail.next = curNode.next;
   curNode.next = oldParentNode;
 
@@ -35,19 +37,19 @@ function reverseLinkedList(head, oldParentNode = head, curNode, tail = head) {
 }
 
 // initialize
-let a = new Node('a');
-let b = new Node('b');
-let c = new Node('c');
-let d = new Node('d');
+// let a = new Node('a');
+// let b = new Node('b');
+// let c = new Node('c');
+// let d = new Node('d');
 
 // assign
-a.next = b;
-b.next = c;
-c.next = d;
+// a.next = b;
+// b.next = c;
+// c.next = d;
 
 // console.log(`Head: ${JSON.stringify(a)}`);
 
 // reverse
-console.log(`New Head: ${JSON.stringify(reverseLinkedList(a))}`);
+// console.log(`New Head: ${JSON.stringify(reverseLinkedList(a))}`);
 
 module.exports = {Node: Node, reverseLinkedList: reverseLinkedList};
