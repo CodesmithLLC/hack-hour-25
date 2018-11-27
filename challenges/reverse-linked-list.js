20f151fdc31cf9562e13cd84e3c5ecbabfa25cc4
Dave Marquess <dave.marquess@gmail.com>
2018-11-27 10:02:43 -0800

    attempt reverse-linked-list

--- challenges/reverse-linked-list.js
+++ challenges/reverse-linked-list.js
@@ -9,12 +9,35 @@
  */
 
 function Node(value) {
-    this.value = value;
-    this.next = null;
+	this.value = value;
+	this.next = null;
 }
 
 function reverseLinkedList(head) {
-
+	if (!head.next) return -1;
+	// starting at head, check next value of every node until next is equal to null
+	let current = head;
+	let newHead;
+	while (current) {
+		newHead = current;
+		// if (!newHead.next) return newHead;
+		current = current.next;
+	}
 }
 
-module.exports = {Node: Node, reverseLinkedList: reverseLinkedList};
+
+const a = new Node('A');
+const b = new Node('B');
+const c = new Node('C');
+const d = new Node('D');
+const e = new Node('E');
+a.next = b;
+b.next = c;
+c.next = d;
+d.next = e;
+// console.log(a.next);
+// console.log(e.next);
+
+console.log(reverseLinkedList(a));
+
+module.exports = { Node: Node, reverseLinkedList: reverseLinkedList };
