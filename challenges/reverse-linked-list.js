d898e72ce17faa6c931109bf19c179be17f130e7
Jim Yoon <jimyoon90@gmail.com>
2018-11-27 10:00:36 -0800

    dont think this works

--- challenges/reverse-linked-list.js
+++ challenges/reverse-linked-list.js
@@ -9,12 +9,35 @@
  */
 
 function Node(value) {
-    this.value = value;
-    this.next = null;
+  this.value = value;
+  this.next = null;
 }
 
-function reverseLinkedList(head) {
-
+function reverseLinkedList(head, newHead) {
+  this.newHead;
+  this.newTail;
+  // base case
+  // if there is no head, return the new linked list
+  if (!head) return this.newHead;
+  // go through the linked list and grab the last link
+  let lastVal;
+  let current = head;
+  while (current.next !== null) {
+    lastVal = current.value;
+    current = current.next;
+  }
+  // if there is no newHead yet, assign it with the first value, which is the last node
+  if (!this.newHead) {
+    this.newHead = new Node(lastVal);
+    // recurse
+    delete current;
+    reverseLinkedList(head, this.newHead);
+  } else {
+    this.newTail = new Node(lastVal);
+    // recurse
+    delete current;
+    reverseLinkedList(head, this.newTail);
+  }
 }
 
 module.exports = {Node: Node, reverseLinkedList: reverseLinkedList};
