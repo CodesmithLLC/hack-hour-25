c63a9e997deaa8ba5eb617bddbd0d8ec7ce72ff9
vwang4536 <vwang4536@gmail.com>
2018-11-27 21:18:52 -0800

    testreverselinkedlist

--- challenges/reverse-linked-list.js
+++ challenges/reverse-linked-list.js
@@ -14,7 +14,15 @@ function Node(value) {
 }
 
 function reverseLinkedList(head) {
-
+    let curr = head;
+    let prev = null;
+    while (curr !== null) {
+        let next = curr.next;
+        curr.next = prev;
+        prev = curr;
+        curr = next;
+    }
+    return curr
 }
 
 module.exports = {Node: Node, reverseLinkedList: reverseLinkedList};
