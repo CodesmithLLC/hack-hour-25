8ed88f763f4840ce01007e82daeba65e72a668a9
Donte' Nall <code4zelda@gmail.com>
2018-11-28 09:14:41 -0800

    update

--- challenges/zip-linked-lists.js
+++ challenges/zip-linked-lists.js
@@ -11,7 +11,18 @@ function Node(val) {
 }
 
 function zip(l1, l2) {
-
+   //declare a count called l1NodesUsed for l1 and l2NodesUsed for l2
+  //if l1's length is 0, return l2
+  //l2's length is 0, return l1
+  //otherwise, set l1s head as the head of the zip list.
+  //compare count to l2's length. If l2's length is exhausted, slice the remaining nodes off of it and add to zip list.
+  //iterate l1NodesUsed count
+  //set l2's head to the next node in the zip list
+  //compare count to l1's length. If l1's length is exhausted, slice the remaining nodes off of it and add to zip list. 
+  //repeat.
+  //return when both are exahusted. 
+ 
+     
 };
 
 module.exports = { Node: Node, zip: zip };
