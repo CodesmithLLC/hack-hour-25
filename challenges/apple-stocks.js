d2f2e4655f7d551ae4c6c128afd727a8f2d65af2
Darryl Amour <darryl.amour@gmail.com>
2018-11-28 10:48:04 -0800

    Updated apple-stocks.js to verify all contents are numbers, that we find the greatest profit by finding the highestPrice with the greatest index in case of duplicates. Re-tested

--- challenges/apple-stocks.js
+++ challenges/apple-stocks.js
@@ -13,28 +13,30 @@
  */
 
 function bestProfit(stock_prices_yesterday) {
-
   // Edge Cases: Check if an array
   if (!(stock_prices_yesterday instanceof Array)) return 0;
 
+  // Edge Cases: All values in array are numbers
+  if (!(stock_prices_yesterday.every(val => typeof val === 'number'))) return 0;
+
   // Find the highest price
   const highestPrice = stock_prices_yesterday.reduce((accum, currentValue, currentIndex) => {
-    if (currentValue > accum.value) {
+    if (currentValue >= accum.value) {
       accum.value = currentValue;
       accum.index = currentIndex;
     }
     return accum;
   }, { index: null, value: Math.max() });
 
-  // Filter lowest Price before highest
+  // Filter lowest Price that occured before highestPrice
   const lowestPrice = stock_prices_yesterday.reduce((accum, currentValue, currentIndex) => {
-    if (currentValue < accum.value && currentIndex <= accum.index) {
+    if (currentValue <= accum.value && currentIndex <= highestPrice.index) {
       accum.value = currentValue;
       accum.index = currentIndex;
     }
     return accum;
   }, { index: highestPrice.index, value: highestPrice.value });
-  
+
   // console.log(highestPrice);
   // console.log(lowestPrice);
 
@@ -42,11 +44,13 @@ function bestProfit(stock_prices_yesterday) {
 }
 
 // let priceList_1 = [];
-// priceList_1[0] = 5;
-// priceList_1[30] = 2;
+// priceList_1[0] = 15;
+// priceList_1[30] = true;
 // priceList_1[40] = 1.50;
-// priceList_1[60] = 10;
-// priceList_1[100] = 2;
+// priceList_1[60] = 3;
+// priceList_1[70] = 0.50;
+// priceList_1[90] = 10;
+// priceList_1[100] = 15;
 
 // console.log(`My Best Profit would be: ${bestProfit(priceList_1)}`);
 
