e592cc8f121ddcaef8c90ce8d7178078f9c3c38f
Jim Yoon <jimyoon90@gmail.com>
2018-11-28 10:01:59 -0800

    couldnt figure out the time difference

--- challenges/apple-stocks.js
+++ challenges/apple-stocks.js
@@ -13,7 +13,32 @@
  */
 
 function bestProfit(stock_prices_yesterday) {
-
+  // check to see if the input is valid
+  if (Array.isArray(stock_prices_yesterday)) {
+    // have variables for low and high price as objects
+    const lowPrice = { value: Infinity, time: 0 };
+    const highPrice = { value: -Infinity, time: 0 };
+    // go through the array and find the lowest and highest stock price
+    for (let i = 0; i < stock_prices_yesterday; i += 1) {
+      // look for low and highs
+      if (stock_prices_yesterday[i] > highPrice.value) {
+        highPrice.value = stock_prices_yesterday[i];
+      }
+      if (stock_prices_yesterday[i] < lowPrice.value) {
+        // if (i >= lowPrice.time) {
+        // }
+        lowPrice.value = stock_prices_yesterday[i];
+      }
+    }
+    // after finding the low and highs, check to see if there is a profit
+    if ((highPrice.time - lowPrice.time) > 0) {
+      return highPrice.value - lowPrice.value;
+    }
+    // if there is no profit, then return 0
+    return 0;
+  }
+  // if it isn't an array, return 0
+  return 0;
 }
 
 module.exports = bestProfit;
