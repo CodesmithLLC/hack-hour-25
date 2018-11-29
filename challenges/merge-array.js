349b7bd854a679f1bf8db0b06f375c7b976a8707
vwang4536 <vwang4536@gmail.com>
2018-11-29 10:15:44 -0800

    test mergearray

--- challenges/merge-array.js
+++ challenges/merge-array.js
@@ -14,7 +14,17 @@
  */
 
 function mergeArrays(arr1, arr2) {
-
+    let i = 0;
+    while (arr2[i]) {
+        if (arr2[i] > arr1[0]) {
+            arr2.splice(i, 0, arr1.shift())
+        }
+        else {i += 1}
+    }
+    return arr2
 }
 
+var my_array = [3,4,6,10,11,15,21];
+var another_array = [1,5,8,12,14,19];
+console.log(mergeArrays(my_array, another_array));// -> [1, 3, 4, 5, 6, 8, 10, 11, 12, 14, 15, 19, 21]
 module.exports = mergeArrays;
