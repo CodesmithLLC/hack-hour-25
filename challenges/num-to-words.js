4af10967d4b91dc6d9bc89e723f27d39bd84b642
Hei Yeung Lam <heiyeunglam@Heis-MacBook-Pro.local>
2018-11-30 10:29:57 -0800

    numtoword

--- challenges/num-to-words.js
+++ challenges/num-to-words.js
@@ -13,7 +13,95 @@
  */
 
 function numToWords(num) {
+  let accum = '';
+  let numObj2 = {
+    1000 : "Thousand",
+    1000000 : "Million",
+    1000000000 : "Billion",
+    1000000000000 : "Quadillion"
+  } 
+  let arr2 = [];
+ for (let i in numObj2) {
+    arr2.push(parseInt(i))
+ }
 
+for (let i = arr2.length - 1; i >= 0 ; i -- ){
+    if (num >= arr2[i]) {
+      let counter = 0;
+      while (num >= arr2[i]) {
+        counter ++;
+        num -= arr2[i]
+      }
+      accum += determineBelowThousand(counter) + numObj2[arr2[i]]
+  }
+}
+  accum +=determineBelowThousand(num)
+  return accum;
 }
 
+function determineBelowHundred(num) {
+  let numObj1 = {
+  1 : "One",
+  2 : "Two", 
+  3 : "Three", 
+  4 : "Four", 
+  5 : "Five", 
+  6 : "Six", 
+  7 : "Seven", 
+  8 : "Eight", 
+  9 : "Nine", 
+  10 : "Ten",  
+  11 : "Eleven", 
+  12 : "Twelve", 
+  13 : "Thirteen", 
+  14 : "Fourteen", 
+  15 : "Fifteen", 
+  16 : "Sixteen", 
+  17 : "Seventeen",
+  18 : "Eighteen",
+  19 : "Nineteen",
+  20 : "Twenty",
+  30 : "Thirty",
+  40 : "Forty",
+  50 : "Fifty",
+  60 : "Sixty",
+  70 : "Seventy",
+  80 : "Eighty",
+  90 : "Ninety"
+  }
+let arr1 = [];
+for (let i in numObj1) {
+  arr1.push(parseInt(i))
+}
+  let accum = '';
+    if (num === 0) return ''
+for (let i = arr1.length - 1; i >= 0 ; i -- ){
+    if (num >= arr1[i]) {
+      accum += numObj1[arr1[i]]
+      num -= arr1[i]
+    }
+  }
+  return accum;
+}
+
+function determineHundred(num) {
+let accum = '';
+  if (num === 0) return ''
+    if (num >= 100) {
+      let counter = 0;
+      while (num >= 100) {
+        counter ++;
+        num -= 100
+      }
+      accum += determineBelowHundred(counter) + "Hundred"
+  }
+  return accum;
+}
+
+function determineBelowThousand(num) {
+  return determineHundred(num) + determineBelowHundred(num % 100);
+}
+
+
+numToWords(300525151340440)
 module.exports = numToWords;
