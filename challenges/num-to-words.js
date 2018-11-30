03c2a06def27df0240186b6517d3a58766da4126
Josie Glore <josieglore@gmail.com>
2018-11-30 10:05:50 -0800

    add num to words

--- challenges/num-to-words.js
+++ challenges/num-to-words.js
@@ -13,7 +13,51 @@
  */
 
 function numToWords(num) {
-
+ const numToArray = num.toString().split('');
+ const smallOptions = {
+   1: 'One',
+   2: 'Two',
+   3: 'Three',
+   4: 'Four',
+   5: 'Five',
+   6: 'Six',
+   7: 'Seven',
+   8: 'Eight',
+   9: 'Nine',
+   10: 'Ten', 
+   11: 'Eleven',
+   12: 'Twelve',
+   13: 'Thirteen',
+   14: 'Fourteen',
+   15: 'Fifteen',
+   16: 'Sixteen',
+   17: 'Seventeen',
+   18: 'Eighteen',
+   19: 'Nineteen',
+ }
+ const mediumOptions = {
+   2: 'Twenty',
+   3: 'Thirty',
+   4: 'Forty',
+   5: 'Fifty', 
+   6: 'Sixty',
+   7: 'Seventy',
+   8: 'Eighty',
+   9: 'Ninety',
+ }
+ const biggerOptions = {
+   3: 'Hundred',
+   4: 'Thousand',
+   5: 'Thousand',
+   6: 'Million',
+   7: 'Million',
+   8: 'Million'
+   9: 'Billion',
+ }
+  if (num < 20) return smallOptions[num];
+  else if (num > 20 && num < 100) {
+    return `${mediumOptions[numToArray[0]]}${smallOptions[numToArray[1]]}`;
+  } 
 }
 
 module.exports = numToWords;
