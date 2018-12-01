617d47c7ac8fa9170bd5c7bec2170a88162b8067
Tony Hudgins <tonyhudgins@gmail.com>
2018-12-01 10:15:13 -0800

    completed perm-palin

--- challenges/perm-palin.js
+++ challenges/perm-palin.js
@@ -10,7 +10,21 @@
  */
 
 function permPalin(str) {
-	
-}
+  // count incidence of characters, only one can be odd
+  const counter = {};
+  for (let i = 0; i < str.length; i++ ) {
+    let char = str[i]
+    if (counter.hasOwnProperty(char)) {
+      counter[char] += 1;
+    }else{
+      counter[char] = 1;
+    }
+  }
+  let oddsCount = 0
+  for (let l in counter) {
+    if (counter[l] % 2) oddsCount += 1; 
+  }
+  return (oddsCount < 2) 
+}  
 
 module.exports = permPalin;
