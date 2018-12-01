1494c9b581a611db92973ea4dbe4fff9334fd2b8
vwang4536 <vwang4536@gmail.com>
2018-12-01 09:54:27 -0800

    permpalin

--- challenges/perm-palin.js
+++ challenges/perm-palin.js
@@ -10,7 +10,15 @@
  */
 
 function permPalin(str) {
-	
+    let newArr = [];
+    let strArr = str.split('').sort();
+    for (let i = 0; i < strArr.length; i += 1) {
+        if (newArr.indexOf(strArr[i]) === -1) { 
+            newArr.push(strArr[i]);
+
+        } else {newArr.pop()};
+    }
+    return (newArr.length <= 1)
 }
 
 module.exports = permPalin;
