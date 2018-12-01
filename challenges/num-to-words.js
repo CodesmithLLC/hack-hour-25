fc2bd05b26e131444032dcfa1237260e2f4e05d9
Darryl Amour <darryl.amour@gmail.com>
2018-12-01 09:33:18 -0800

    in progress for solving num-to-words.js, incomplete and untested

--- challenges/num-to-words.js
+++ challenges/num-to-words.js
@@ -1,4 +1,7 @@
-// Write a function that takes a number as an argument and returns its english word representation as a string. Answers should be in upper camel case (a.k.a. Pascal case). Don't worry about spaces.
+// Write a function that takes a number as an argument and 
+// returns its english word representation as a string.
+// Answers should be in upper camel case (a.k.a. Pascal case).
+// Don't worry about spaces.
 // Include support for 11-19 ('Eleven', 'Twelve', 'Thirteen', ... 'Nineteen').
 
 
@@ -13,7 +16,31 @@
  */
 
 function numToWords(num) {
+  let answer = '';
+  let digitFinderRegExp;
+  const ones = [
+    'Zero', 'One', 'Two', 'Three', 'Four',
+    'Five', 'Six', 'Seven', 'Eight', 'Nine'];
+  const tens = [
+    '', 'Ten', 'Twenty', 'Thirty', 'Forty', 'Fifty',
+    'Sixty', 'Seventy', 'Eighty', 'Ninety'];
 
+  // Edge Case: if argument is not a number
+  if (!(typeof num === 'number')) return NaN;
+
+  // Edge Case: Validate if number is larger than our current support
+  if (num > 99) return 'Argument value too large. Currently support (0-99)';
+
+  // Process Tens
+  digitFinderRegExp = RegExp(/\d{1,2}/,'g');
+  answer += tens[digitFinderRegExp.exec(num)];
+
+  // Process Ones
+  digitFinderRegExp = RegExp(/[0-9]{1,1}$/,'g');
+  answer += ones[digitFinderRegExp.exec(num)];
+
+  // return result
+  return answer;
 }
 
 module.exports = numToWords;
