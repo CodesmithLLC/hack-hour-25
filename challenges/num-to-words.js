633534eaf41b55430a539bdc875ddc6de2975abe
chrisfranz <chrisfranz@mac.com>
2018-12-01 09:47:03 -0800

    completed

--- challenges/num-to-words.js
+++ challenges/num-to-words.js
@@ -14,6 +14,53 @@
 
 function numToWords(num) {
 
+  const cache = {
+    0: 'Zero',
+    1: 'One',
+    2: 'Two',
+    3: 'Three',
+    4: 'Four',
+    5: 'Five',
+    6: 'Six',
+    7: 'Seven',
+    8: 'Eight',
+    9: 'Nine',
+    10: 'Ten',
+    11: 'Eleven',
+    12: 'Twelve',
+    13: 'Thirteen',
+    14: 'Fourtenn',
+    15: 'Fifteen',
+    16: 'Sixteen',
+    17: 'Seventeen',
+    18: 'Eighteen',
+    19: 'Ninetenn',
+    20: 'Twentty',
+    30: 'Thirty',
+    40: 'Fourty',
+    50: 'Fifty',
+    60: 'Sixty',
+    70: 'Seventy',
+    80: 'Eighty',
+    90: 'Ninety',
+    '1H': 'Hundred',
+    '1K': 'Thousand',
+    '1M': 'Million',
+    '1B': 'Billion',
+    '1T': 'Trillion',
+    '1Q': 'Quadrillion'
+  }
+
+    let output = '';
+
+    if (cache[num]) return cache[num];
+
+
+
+    console.log(digits);
+
 }
+
+console.log(numToWords(44));
 
 module.exports = numToWords;
--- challenges/perm-palin.js
+++ challenges/perm-palin.js
@@ -10,7 +10,19 @@
  */
 
 function permPalin(str) {
-	
+  // split string into array of characaters
+  const chars = str.split('');
+  // iterate through chars to create an object with character as key, number of occurances as value
+  const charCount = chars.reduce((accum, element) => {
+    accum[element] = (accum[element] || 0) + 1;
+    return accum;
+  }, {})
+  // iterate through charCount's entries to create an array of all characters with odd number of occurances
+  const charCheck = Object.entries(charCount).filter((element) => {
+    return element[1] % 2 !== 0
+  })
+  // if array has more than one item (input string has more than one odd character occurance), return false
+  return charCheck.length <= 1;
 }
 
 module.exports = permPalin;
