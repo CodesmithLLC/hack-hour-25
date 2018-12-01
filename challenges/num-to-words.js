a834d1c9b425566ce057a9a3ec2a47ebced979d8
Jae Hyun Lee <jaehyunlee@Jaes-MacBook-Pro-2.local>
2018-12-01 09:45:50 -0800

    fixed num to words and perm palin

--- challenges/num-to-words.js
+++ challenges/num-to-words.js
@@ -15,7 +15,7 @@
 function numToWords(num) {
 
 	// regular words
-	const words = ['Zero', 'One', 'Two', 'Three', 'Four', 'Five', 'Six', 'Seven', 'Eight', 'Nine', 'Ten', 'Eleven', 'Twelve', 'Thirteen', 'Fourteen', 'Fifteen', 'Sixteen', 'Seventeen', 'Eighteen', 'Nineteen'];
+	const words = ['', 'One', 'Two', 'Three', 'Four', 'Five', 'Six', 'Seven', 'Eight', 'Nine', 'Ten', 'Eleven', 'Twelve', 'Thirteen', 'Fourteen', 'Fifteen', 'Sixteen', 'Seventeen', 'Eighteen', 'Nineteen'];
 	
 	// the tens value
 	const wordsTy = ['', '', 'Twenty', 'Thirty', 'Forty', 'Fifty', 'Sixty', 'Seventy', 'Eighty', 'Ninty'];
@@ -28,33 +28,45 @@ function numToWords(num) {
 	let index = 0;
 	let placeIndex = 0;
 
+	if (num === 0) return 'Zero';
+
 	while (numArray.length !== 0) {
 
 		const curArray = numArray.splice(index, index + 3).reverse();
 		let currStr = '';
+		let setPlace = false;
 
 		if (curArray.length === 3) {
-			currStr = words[curArray[0] * 1] + 'Hundred';
-			if (((curArray[1] + curArray[2])*1) < 20) {
-				currStr += words[(curArray[1] + curArray[2])*1];
-			} else {
-				currStr += wordsTy[curArray[1] * 1] + words[curArray[2] * 1];
+			if (curArray[0] !== '0') currStr = words[curArray[0] * 1] + 'Hundred';
+			if (curArray[1] + curArray[2] !== '00') { 
+				setPlace = true;
+				if (((curArray[1] + curArray[2])*1) < 20) {
+					currStr += words[(curArray[1] + curArray[2])*1];
+				} else {
+					currStr += wordsTy[curArray[1] * 1] + words[curArray[2] * 1];
+				}
 			}
 		}
 
 		if (curArray.length === 2) {
-			if (((curArray[0] + curArray[1])*1) < 20) {
-				currStr += words[(curArray[0] + curArray[1])*1];
-			} else {
-				currStr += wordsTy[curArray[0] * 1] + words[curArray[1] * 1];
+			if ((curArray[0] + curArray[1]) !== '00') {
+				setPlace = true;
+				if (((curArray[0] + curArray[1])*1) < 20) {
+					currStr += words[(curArray[0] + curArray[1])*1];
+				} else {
+					currStr += wordsTy[curArray[0] * 1] + words[curArray[1] * 1];
+				}
 			}
 		}
-
+			
 		if (curArray.length === 1) {
-			currStr = words[curArray[0] * 1];
+			if (curArray[0] !== '0'){
+				setPlace = true;
+				currStr = words[curArray[0] * 1];
+			}
 		}
 		
-		currStr += places[placeIndex];
+		if (setPlace) currStr += places[placeIndex];
 		placeIndex += 1;
 		result = currStr + result;
 	}
--- challenges/perm-palin.js
+++ challenges/perm-palin.js
@@ -10,12 +10,15 @@
  */
 
 function permPalin(str) {
+	if (Str.length === 0) return false;
 	if (str.length === 1) return true;
-	if (str.length === 2) (str[0] === str[1] ? true : false);
+	if (str.length === 2 && str[0] === str[1]) return true;
+
 	for (let i = 1; i < str.length - 1; i += 1) {
 		if (str[i - 1] === str[i + 1]) return true;
-		//if (str[i] === str[i + 1]) return true;
+		if (str[i] === str[i + 1]) return true;
 	}
+	
 	return false;
 }
 
