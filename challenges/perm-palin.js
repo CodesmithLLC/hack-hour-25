f90799647a26d5e3f14a8b42cc3955660d824738
evgenii-codesmith <evgenii.codesmith@gmail.com>
2018-12-01 09:56:42 -0800

    Perm-palin solution

--- challenges/perm-palin.js
+++ challenges/perm-palin.js
@@ -10,7 +10,30 @@
  */
 
 function permPalin(str) {
-	
+	let hashTable = {};
+	let count = 0;
+	for (let i = 0; i < str.length; i++){
+		hashTable[str[i]] = hashTable[str[i]]+1 || 1;
+	}
+
+	for (let key in hashTable){
+		
+		if (hashTable[key] %2 !==0) {
+			count++;
+			
+		}
+	}
+
+		if (count >1) return false;
+
+	return true;
 }
+
+ console.log(permPalin('abab'));// => true
+ console.log(permPalin('cbaba'));// => true
+ console.log(permPalin('a'));
+ console.log(permPalin('cbac'));// => false
+ // permPalin('a');// => true
+
 
 module.exports = permPalin;
