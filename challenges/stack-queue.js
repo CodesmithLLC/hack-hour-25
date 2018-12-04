b088535483d260e44887cc76b88fe6272e7efb17
Adrian <34041016+Sgshi@users.noreply.github.com>
2018-12-04 10:04:00 -0800

    finished stack queu for real this time

--- challenges/stack-queue.js
+++ challenges/stack-queue.js
@@ -22,17 +22,39 @@ class Stak {
 
 class Quee {
   constructor(){
-    this.array = []
+    this.stack1 = new Stak()
+    this.stack2 = new Stak()
   }
   enqueue(val){
-    this.array.push(val);
+    this.stack1.push(val);
   }
   dequeue(){
-    return this.array.shift();
+    const length = this.stack1.array.length
+    for(let i = 0; i<length; i+=1){
+      this.stack2.push(this.stack1.pop());
+      
+    }
+    const output = this.stack2.pop();
+    for(let i = 0; i<length-1; i+=1){
+      this.stack1.push(this.stack2.pop());
+    }
+    return output;
   }
 }
 
-const Stack = new Stak;
-const Queue = new Quee;
+const q = new Quee();
+q.enqueue(1)
+q.enqueue(2)
+q.enqueue(3)
+q.enqueue(4)
+q.enqueue(5)
+q.dequeue();
+q.dequeue();
+q.dequeue();
+q.dequeue();
+
+const Stack = new Stak();
+const Queue = new Quee();
+
 
 module.exports = { Stack: Stack, Queue: Queue };
