2075c5ec2371f8386df3e18ab5c3723f96953b6a
Jason Wong <jasonwong@Jasons-MacBook-Pro.local>
2018-12-04 10:11:48 -0800

    stack-queue

--- challenges/stack-queue.js
+++ challenges/stack-queue.js
@@ -4,8 +4,22 @@
 
 
 function Stack() {
-
+  this.storage = {};
+  this.index = 0;
 }
+
+Stack.prototype.enqueue = function(value) {
+  this.storage[this.index] = value;
+  this.index += 1;
+  return value;
+};
+
+Stack.prototype.dequeue = function() {
+  if (this.index > 0) {
+    this.index -= 1;
+    return this.storage[this.index];
+  }
+};
 
 
 /**
@@ -14,7 +28,21 @@ function Stack() {
 
 
 function Queue() {
-
+  this.beginning = new Stack();
+  this.ending = new Stack();
 }
+
+Queue.prototype.enqueue = function(value) {
+  this.ending.enqueue(value);
+};
+
+Queue.prototype.dequeue = function() {
+  if (this.beginning.index === 0) {
+    while (this.ending.index > 0) {
+      this.beginning.enqueue(this.ending.dequeue());
+    }
+  }
+  return this.beginning.dequeue();
+};
 
 module.exports = {Stack: Stack, Queue: Queue};
