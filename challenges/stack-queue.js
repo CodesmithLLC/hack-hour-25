49b00c2aa9df249f69572a3eb9a195d57136d535
Tony Hudgins <tonyhudgins@gmail.com>
2018-12-04 15:03:34 -0800

    finished stack queue

--- challenges/stack-queue.js
+++ challenges/stack-queue.js
@@ -4,17 +4,49 @@
 
 
 function Stack() {
-
+  const arr = [];
+  return {
+    push: (e) => {
+      arr.push(e)
+      return ;
+    },
+    pop: () => {
+      return arr.pop();
+    }
+  } 
 }
-
 
 /**
 * Queue Class
 */
 
-
 function Queue() {
-
+  let stack1 = new Stack,
+      stack2 = new Stack;
+  return {
+    enqueue: (e) => {
+      stack1.push(e);
+    },
+    dequeue: () => {
+    	let e;
+    	while(true) {
+	    	e = stack1.pop()
+    		if (e === undefined) {
+    			break
+    		}
+  			stack2.push(e)
+		  }
+			let res = stack2.pop()
+    	while(true) {
+	    	e = stack2.pop()
+    		if (e === undefined) {
+    			break
+    		}
+  			stack1.push(e)
+		  }
+      return res
+    }
+	}
 }
 
 module.exports = {Stack: Stack, Queue: Queue};
