6dbece117917ad995d1876bb83145989848f2687
codemccodeface <yurenfrankma@gmail.com>
2018-12-04 10:19:28 -0800

    finished stack-queue

--- challenges/num-to-words.js
+++ challenges/num-to-words.js
@@ -12,8 +12,71 @@
  * numToWords(92120000000000000) -> 'NintyTwoQuadrillionOneHundredTwentyTrillion'
  */
 
-function numToWords(num) {
+const ONES_PLACE = ['', 'One', 'Two', 'Three', 'Four', 'Five', 'Six', 'Seven', 'Eight', 'Nine'];
+const TENS_PLACE = ['', 'Ten', 'Twenty', 'Thirty', 'Forty', 'Fifty', 'Sixty', 'Seventy', 'Eighty', 'Ninety'];
+const ALT_TENS = ['Ten', 'Eleven', 'Twelve', 'Thirteen', 'Fourteen', 'Fifteen', 'Sixteen', 'Seventeen', 'Eighteen', 'Nineteen'];
+const COARSE_PLACES = ['', 'Thousand', 'Million', 'Billion', 'Trillion', 'Quadrillion'];
 
+function numToWords(num) {
+  if (num === 0) return 'Zero';
+  
+  // convert number into array of three digit strings,
+  // with ascending order of place value
+  const numStrs = chunkNumber(num);
+  
+  // turn array of number strings into array of words
+  const finalNumStrs = numStrs.map((numStr, i) => {
+    // create word from three digits
+    const threeDigit = parseThreeDigit(numStr);
+    // if word is nonempty, add thousands/millions place value
+    return threeDigit && threeDigit + COARSE_PLACES[i];
+  });
+  
+  // Reverse array of words to be in descending place value, then join together
+  return finalNumStrs.reverse().join('');
+}
+
+// Takes in a number, and returns an array of three digit strings
+// The place value of these strings is in ascending order
+function chunkNumber(num) {
+  // create number string
+  let numStr = num.toString();
+  
+  // pad number string with frontal zeroes, so that its length is a multiple of 3
+  const remainder = numStr.length % 3;
+  const frontalZeroes = remainder && 3 - remainder;
+  numStr = '0'.repeat(frontalZeroes) + numStr;
+  
+  // create groups of three digit strings
+  const numStrs = [];
+  let j = numStr.length;
+  while (j > 0) {
+    const i = j - 3;
+    
+    const threeDigits = numStr.slice(i, j);
+    numStrs.push(threeDigits);
+    
+    j = i;
+  }
+  
+  return numStrs;
+}
+
+// create word from three digits, not including thousands/millions place value
+function parseThreeDigit(numStr) {
+  let result = '';
+  
+  // parse hundreds place
+  if (numStr[0] !== '0')
+    result += ONES_PLACE[numStr[0]] + 'Hundred';
+  // parse tens/once place for non-teen numbers
+  if (numStr[1] !== '1')
+    result += TENS_PLACE[numStr[1]] + ONES_PLACE[numStr[2]];
+  // parse tens/once place for teen numbers
+  else
+    result += ALT_TENS[numStr[2]];
+  
+  return result;
 }
 
 function numToWordsTriple(num) {
--- challenges/stack-queue.js
+++ challenges/stack-queue.js
@@ -2,19 +2,53 @@
  * Create a stack.Then create a queue using two stacks.
  */
 
-
+// stack: first in last out; push, pop
 function Stack() {
-
+  this.data = [];
+  this.length = 0;
+  this.push = (e) => {
+    this.length += 1;
+    this.data.push(e);
+  };
+  this.pop = () => {
+    this.length -= 1;
+    return this.data.pop();
+  };
 }
-
 
 /**
 * Queue Class
 */
 
-
+// queue: first in first out; enqueue, dequeue
 function Queue() {
-
+  this.stack1 = new Stack();
+  this.stack2 = new Stack();
+  this.enqueue = e => this.stack1.push(e);
+  this.dequeue = () => {
+    let times = this.stack1.length;
+    for (let i = 0; i < times; i += 1) {
+      this.stack2.push(this.stack1.pop());
+    }
+    const val = this.stack2.pop();
+    times = this.stack2.length;
+    for (let i = 0; i < times; i += 1) {
+      this.stack1.push(this.stack2.pop());
+    }
+    return val;
+  };
 }
+
+let queue1 = new Queue();
+queue1.enqueue(1);
+queue1.enqueue(2);
+queue1.enqueue(3);
+queue1.enqueue(4);
+queue1.enqueue(5);
+console.log(queue1.dequeue());
+console.log(queue1.dequeue());
+console.log(queue1.dequeue());
+console.log(queue1.dequeue());
+console.log(queue1.dequeue());
 
 module.exports = {Stack: Stack, Queue: Queue};
