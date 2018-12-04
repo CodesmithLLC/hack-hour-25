bbb763f3da3e9a1d46e104db7bc1e6e52b600a53
Jim Yoon <jimyoon90@gmail.com>
2018-12-04 09:58:06 -0800

    dont know if this works

--- challenges/stack-queue.js
+++ challenges/stack-queue.js
@@ -4,8 +4,22 @@
 
 
 function Stack() {
-
+  this.storage = [];
+  this.count = 0;
 }
+
+Stack.prototype.push = (val) => {
+  this.storage[this.count] = val;
+  this.count += 1;
+};
+
+Stack.prototype.pop = () => {
+  if (this.count === 0) {
+    return undefined;
+  }
+  this.count -= 1;
+  return this.storage.pop();
+};
 
 
 /**
@@ -14,7 +28,25 @@ function Stack() {
 
 
 function Queue() {
-
+  this.stack1 = Stack();
+  this.stack2 = Stack();
 }
+
+Queue.prototype.enqueue = (val) => {
+  this.stack1.push(val);
+};
+
+Queue.prototype.dequeue = () => {
+  if (this.stack2.count === 0) {
+    if (this.stack1.count === 0) {
+      return 'Error, stacks are empty';
+    }
+    while (this.stack1.count > 0) {
+      let holder = this.stack1.pop();
+      this.stack2.push(holder);
+    }
+  }
+  return this.stack2.pop();
+};
 
 module.exports = {Stack: Stack, Queue: Queue};
