afb3412a2fa5be8697b76a6b0f3052eb01d39cf0
Jae Hyun Lee <jaehyunlee@Jaes-MacBook-Pro-2.local>
2018-12-04 09:48:27 -0800

    stack-queue

--- challenges/stack-queue.js
+++ challenges/stack-queue.js
@@ -4,17 +4,39 @@
 
 
 function Stack() {
-
+	return [];	
 }
-
 
 /**
 * Queue Class
 */
 
-
 function Queue() {
+	const stack1 = new Stack();
+	const stack2 = new Stack();
 
+	this.queue = (element) => {
+		// add element to end of stack1
+		stack1.push(element);
+		return element;
+	}
+	
+	this.dequeue = () => {
+		if (stack1.length === 0) return null;
+		// 'pour' stack1 unto stack2 until the very first element of stack1 is left
+		while (stack1.length !== 1 && stack1.length !== 0) {
+			stack2.push(stack1.pop());
+		}
+		// pop the result we want
+		const result = stack1.pop();
+		// 'pour' stack2 unto stack1
+		while (stack2.length !== 1 && stack2.length !== 0) {
+			stack1.push(stack2.pop());
+		}
+		// return element that was dequeued
+		return result;
+	}
+	return stack1;
 }
 
 module.exports = {Stack: Stack, Queue: Queue};
