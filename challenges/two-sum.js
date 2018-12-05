bfb06b453729eec089ed503b593a567c12ebf184
Josie Glore <josieglore@gmail.com>
2018-12-05 10:05:46 -0800

    add two sum

--- challenges/two-sum.js
+++ challenges/two-sum.js
@@ -2,8 +2,23 @@
  *  array that sum up to the target value; return false otherwise
  */
 
-function twoSum(arr, n) {
+// function twoSum(arr, n) {
+//   for (let i = 0; i < arr.length; i += 1) {
+//     for (let j = 1; j < arr.length; j += 1) {
+//       if (i !== j) {
+//         if (arr[i] + arr[j] === n) return true;
+//       }
+//     }
+//   }
+//   return false;
+// }
 
+
+function twoSum(arr, n, i = 0, j = i + 1) {
+  if (i === arr.length - 1) return false;
+  if (n - arr[i] === arr[j]) return true;
+  if (j < arr.length) return twoSum(arr, n, i, j + 1);
+  return twoSum(arr, n, i + 1, j = 1);
 }
 
 module.exports = twoSum;
