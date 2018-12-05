68c3cbbf2ce5650e851f8177dcb9576203b36749
Jason Wong <jasonwong@Jasons-MacBook-Pro.local>
2018-12-05 09:45:18 -0800

    twosum

--- challenges/two-sum.js
+++ challenges/two-sum.js
@@ -3,7 +3,23 @@
  */
 
 function twoSum(arr, n) {
-
+  const sorted = arr.sort((a, b) => a - b);
+  let end = sorted.length - 1;
+  let beginning = 0;
+  while (beginning < end) {
+    const sum = sorted[beginning] + sorted[end];
+    if (sum === n) {
+      return true;
+    }
+    if (sum < n) {
+      beginning += 1;
+    }
+    if (sum > n) {
+      end -= 1;
+    }
+  }
+  return false;
 }
+
 
 module.exports = twoSum;
