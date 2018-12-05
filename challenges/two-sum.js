047ada6b8d165a548fd05a5af635417de69041c8
Adrian <34041016+Sgshi@users.noreply.github.com>
2018-12-05 09:42:40 -0800

    two-sum

--- challenges/two-sum.js
+++ challenges/two-sum.js
@@ -3,7 +3,13 @@
  */
 
 function twoSum(arr, n) {
-
+  const array = arr.filter(num => num<=n);
+  for(let i = 0; i<array.length; i+=1){
+    for(let j = 1; j<array.length; j+=1){
+      if(array[j]+array[i] === n)return true;
+    }
+  }
+  return false;
 }
 
 module.exports = twoSum;
