973cbc665a457da9b8892bf9352b05e8667c2e19
Dave Marquess <dave.marquess@gmail.com>
2018-12-07 09:54:20 -0800

    complete gcd

--- challenges/gcd.js
+++ challenges/gcd.js
@@ -8,7 +8,27 @@
  */
 
 function gcd(a, b) {
-
+  // create highestDiv variable to store greatest common divisor
+  let highestDiv = 0;
+  // check if a is greater than b. if so,
+  if (a > b) {
+    // iterate from 1 up to a
+    for (let i = 0; i < a; i += 1) {
+      // on each iteration, check if a divided by current num yields 
+      // remainder of 0 and b divided by num yields remainder of 0.
+      // if so, set highestDiv equal to current num
+      if (a % i === 0 && b % i === 0) highestDiv = i;
+    }
+  } else {
+    // otherwise, do same, but going up to b
+    for (let i = 0; i < b; i += 1) {
+      // on each iteration, check if a divided by current num yields 
+      // remainder of 0 and b divided by num yields remainder of 0.
+      // if so, set highestDiv equal to current num
+      if (a % i === 0 && b % i === 0) highestDiv = i;
+    }
+  }
+  return highestDiv;
 }
 
 module.exports = gcd;
