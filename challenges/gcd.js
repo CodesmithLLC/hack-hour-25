882f539d02aded2f5ad1152873cc8f3680c02a22
Hei Yeung Lam <heiyeunglam@Heis-MacBook-Pro.local>
2018-12-07 09:42:25 -0800

    gcd

--- challenges/gcd.js
+++ challenges/gcd.js
@@ -6,9 +6,20 @@
  * gcd(10, 9) -> 1
  *
  */
-
 function gcd(a, b) {
-
+  if (a === b) return a
+  if (a < b) {
+    let temp = a;
+    a = b;
+    b = temp;
+  }
+	let result = 1;
+	for (let i = 2; i <= b; i ++) {
+		if ( a % i === 0 && b % i ===0) {
+			result = i
+		}
+	}
+	return result;
 }
 
 module.exports = gcd;
