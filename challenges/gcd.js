cb44a82048dd50dd46c2f99dd06d4136ca44aec7
Jae Hyun Lee <jaehyunlee@Jaes-MacBook-Pro-2.local>
2018-12-07 09:32:56 -0800

    gcd

--- challenges/gcd.js
+++ challenges/gcd.js
@@ -8,7 +8,25 @@
  */
 
 function gcd(a, b) {
+	const aDivisors = new Set();
+	const bDivisors = new Set();
 
+	for (let i = 0; i <= a; i += 1) {
+		if (a % i === 0) aDivisors.add(i);
+	}
+	for (let x = 0; x <= b; x += 1) {
+		if (b % x === 0) bDivisors.add(x);
+	}
+
+	let length = 0;
+	if (a < b) length = a;
+	else length = b;
+
+	let result = 0;
+	for (let y = 0; y <= length; y += 1) {
+		if (aDivisors.has(y) && bDivisors.has(y) && y > result) result = y; 
+	}
+	return result;
 }
 
 module.exports = gcd;
