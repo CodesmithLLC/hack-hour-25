e1e2da714bb7d58a374ec2315b8a8bad704fcf02
Adrian <34041016+Sgshi@users.noreply.github.com>
2018-12-07 09:49:48 -0800

    gcd.js

--- challenges/gcd.js
+++ challenges/gcd.js
@@ -8,7 +8,22 @@
  */
 
 function gcd(a, b) {
+  if(a<1||b<1)return 0;
 
+  let hold = Math.max(a,b);
+  while(hold !== 0){
+    if(a%hold ===0 && b%hold===0){
+      break;
+    }
+    hold = hold-1;
+  }
+  return hold;
 }
+
+
+console.log(gcd (100,10));
+console.log(gcd (8,2));
+console.log(gcd (90,9));
+console.log(gcd (1,0));
 
 module.exports = gcd;
