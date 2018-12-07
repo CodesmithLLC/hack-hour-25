1b2b1e5839b157db5400087b421522bcce62acdd
Tony Hudgins <tonyhudgins@gmail.com>
2018-12-07 10:02:47 -0800

    completed gcd

--- challenges/gcd.js
+++ challenges/gcd.js
@@ -8,7 +8,17 @@
  */
 
 function gcd(a, b) {
-
+  let  smaller = Math.min(a, b), bigger = Math.max(a, b)
+  for (let step = 1, gcd = smaller; step < smaller; step) {
+    if ( bigger % gcd === 0 ) return gcd 
+    while(step++) {
+      if (smaller / step < 2) return 1
+      if (smaller % step === 0) {
+        gcd = smaller / step; 
+        break;
+      }
+    }
+  }
 }
 
 module.exports = gcd;
