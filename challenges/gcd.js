3c8b12520642ce9e898383a2ba121cdca3704094
codemccodeface <yurenfrankma@gmail.com>
2018-12-13 13:51:00 -0800

    didit

--- challenges/gcd.js
+++ challenges/gcd.js
@@ -7,8 +7,36 @@
  *
  */
 
+
+
 function gcd(a, b) {
-
+  // start with the smaller of the two, check if it divides into other
+  const smaller = Math.min(a, b);
+  const bigger = Math.max(a, b);
+  const smallerDenoms = [];
+  // start with the smaller of the two
+  // increment a counter from 1 until sqrt of smaller
+  let increment = 1;
+  
+  // keep incrementing until increment < sqrt of smaller
+  while (increment <= Math.sqrt(smaller)) {
+    const currentDivisor = smaller / increment;
+    const commonDenom = bigger / currentDivisor;
+    console.log('commonDenom: ', commonDenom);
+    if (commonDenom % 1 === 0) return currentDivisor;
+    if (currentDivisor % 1 === 0) smallerDenoms.push(increment);
+    increment += 1;
+  }
+  while (smallerDenoms.length > 0) {
+    const potential = smallerDenoms.pop();
+    const divides = bigger / potential;
+    if (divides % 1 === 0) return potential;
+  }
+  return 1;
+  // set the current divisor as start / increment
+  // set current common denominator to check as bigger/currentDivisor
+  // as long as commonDenom is not a whole number, keep incrementing increment
+  // if reached up until sqrt and nothing, return 1.
 }
-
+console.log(gcd(24, 16));
 module.exports = gcd;
--- challenges/valid-bst.js
+++ challenges/valid-bst.js
@@ -7,13 +7,34 @@
  
 
 function BinaryTree(val) {
-    this.value = val;
-    this.left = null;
-    this.right = null;
+  this.value = val;
+  this.left = null;
+  this.right = null;
 }
 
-function validBST(tree) {
-
+function validBST(tree, min = -Infinity, max = Infinity) {
+  if (tree.value < min || tree.value > max) return false;
+  if (tree.left && tree.right) return validBST(tree.left, min, tree.value) && validBST(tree.right, tree.value, max);
+  if (tree.right) return validBST(tree.right, tree.value, max);
+  if (tree.left) return validBST(tree.left, min, tree.value);
+  return true;
 }
+
+const node6 = new BinaryTree(6);
+const node4 = new BinaryTree(4);
+const node10 = new BinaryTree(10);
+const node5 = new BinaryTree(5);
+const node8 = new BinaryTree(8);
+const node16 = new BinaryTree(16);
+const node7 = new BinaryTree(7);
+
+node6.left = node4;
+node6.right = node10;
+node4.right = node5;
+node10.left = node8;
+node10.right = node16;
+node8.right = node7;
+
+console.log(validBST(node6));
 
 module.exports = {BinaryTree: BinaryTree, validBST: validBST};
