99ab17203c36e267ac8d75ae3c36edae22396ea8
Jim Yoon <jimyoon90@gmail.com>
2018-12-17 09:35:50 -0800

    finished count-twos challenge

--- challenges/count-twos.js
+++ challenges/count-twos.js
@@ -10,7 +10,25 @@
 
 
 function countTwos(num) {
-
+  let counter = 0;
+  // go through all the numbers up to the num
+  for (let i = 1; i <= num; i += 1) {
+    // take each number and turn it into a string to check each digit
+    const holder = i.toString().split('');
+    // go through the array and check each element/digit
+    for (let j = 0; j < holder.length; j += 1) {
+      if (holder[j] === '2') {
+        counter += 1;
+      }
+    }
+  }
+  return counter;
 }
+
+console.log(countTwos(1));  // -> 0
+console.log(countTwos(3));  // -> 1
+console.log(countTwos(13));  // -> 2
+console.log(countTwos(1000));  // -> 300
+console.log(countTwos(11420));  // -> 4483
 
 module.exports = countTwos;
