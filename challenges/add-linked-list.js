cf5be2ffa5c279cf925cbe4f8ca75896ce489c22
Josie Glore <josieglore@gmail.com>
2018-12-18 09:51:31 -0800

    add add linked list

--- challenges/add-linked-list.js
+++ challenges/add-linked-list.js
@@ -18,7 +18,71 @@ function Node(val) {
 }
 
 function addLinkedList(l1, l2) {
+  // let num1 = '';
+  // let num2 = '';
+  let currentNode1 = l1.head;
+  let num1 = (currentNode1.value).toString();
+  let currentNode2 = l2.head;
+  let num2 = (currentNode2.value).toString();
+  while (currentNode1.next && currentNode2.next) {
+    if (l1) {
+      currentNode1 = currentNode1.next;
+      num1 += currentNode1.value; 
 
+    }
+    if (l2) {
+      currentNode2 = currentNode2.next;
+      num2 += currentNode2.value;
+    }
+  }
+  // console.log(num1)
+  // console.log(num2)
+  const l3Values = (Number(num1) + Number(num2)).toString().split('');
+  console.log(l3Values)
+  // const l3 = new Node(l3Values[l3Values.length - 1]);
+  // for (let i = l3Values.length - 1; i >= 0; i -= 1) {
+  //   let l3 = new Node(l3Values[i])
+  // }
+  let i = l3Values.length - 1;
+  const l3 = new Node(Number(l3Values[i]));
+  let l3Next = l3;
+  console.log(l3Next)
+  console.log(l3)
+  while (i > 0) {
+    i -= 1;
+    l3Next.next = new Node(Number(l3Values[i]))
+    l3Next = l3Next.next;
+    // l3Next.next = l3Next;
+    console.log(l3)
+    // l3Current = l3Current.next;
+  }
+  return l3;
 }
+// function LinkedList(...args) {
+//   this.head = null;
+//   this.tail = null;
+// }
+// LinkedList.prototype.push = function(value) {
+//   const node = new Node(value);
+//   if (this.head === null) {
+//     this.head = node;
+//     this.tail = node;
+//   }
+//   else {
+//     this.tail.next = node;
+//     this.tail = node;
+//   }
+// }
 
+// const l1 = new LinkedList();
+// const l2 = new LinkedList();
+
+// l1.push(2)
+// l1.push(1)
+// l1.push(5)
+// l2.push(5)
+// l2.push(9)
+// l2.push(2)
+
+// console.log(addLinkedList(l1, l2))
 module.exports = {Node: Node, addLinkedList: addLinkedList};
