3e4a198d842cf6994bf9d30332bd3a22f96f828e
Dave Marquess <dave.marquess@gmail.com>
2018-12-18 10:09:45 -0800

    attempt add-linked-list

--- challenges/add-linked-list.js
+++ challenges/add-linked-list.js
@@ -1,4 +1,5 @@
-/* You have two numbers represented by linked lists. Each node contains a single digit. The digits
+/* You have two numbers represented by linked lists. Each node contains a single digit.
+// The digits
  * are stored in reverse order, such that the 1's digit is at the head of the list. Write
  * a function that adds the two numbers and returns the sum as a linked list with the same
  * structure.
@@ -18,7 +19,31 @@ function Node(val) {
 }
 
 function addLinkedList(l1, l2) {
+  // create temp carryOver variable to store carry over if addedNum is greater than 10
+  let carryOver = 0;
+  // iterate thru l1 and l2 nodes, adding current value to new node value's next
+  const newNode = new Node(l1.value + l2.value);
 
+  let current1 = l1;
+  let current2 = l2;
+  let newNodeNext = newNode.next;
+
+  while (current1) {
+    current1 = l1.next;
+    current2 = l2.next;
+    newNode.next = new Node(current1.value + current2.value);
+  }
+  return newNode;
 }
 
-module.exports = {Node: Node, addLinkedList: addLinkedList};
+const node1 = new Node(2);
+node1.next = new Node(1);
+const node2 = new Node(5);
+node2.next = new Node(9);
+
+console.log(addLinkedList(node1, node2));
+// console.log(5)
+
+// Input: (2 -> 1 -> 5) + (5 -> 9 -> 2)
+// Output: 7 -> 0 -> 8
+module.exports = { Node: Node, addLinkedList: addLinkedList };
--- challenges/count-twos.js
+++ challenges/count-twos.js
@@ -1,6 +1,9 @@
-// Write a function "countTwos" that takes a number as an argument, n. The function will return the number of 2s encountered when counting from 1 to n (inclusive). 
-// For example: countTwos(3) should be 1 because there are no 2s in the number one. There is One 2 in the number two and no 2s in the number three.
-// Each 2 should be counted. For instance,  the number 2222 has four 2s. 
+// Write a function "countTwos" that takes a number as an argument, n.
+// The function will return the number of 2s encountered when counting
+// from 1 to n (inclusive).
+// For example: countTwos(3) should be 1 because there are no 2s in the
+// number one. There is One 2 in the number two and no 2s in the number three.
+// Each 2 should be counted. For instance,  the number 2222 has four 2s.
 
 // countTwos(1);  -> 0
 // countTwos(3);  -> 1
@@ -8,9 +11,35 @@
 // countTwos(1000);  -> 300
 // countTwos(11420);  -> 4483
 
-
 function countTwos(num) {
-
+  if (num <= 1) return 0;
+  // create variable to count numOfTwos
+  let numOfTwos = 0;
+  // split num into array of nums
+  const numArray = num.toString().split('').map(numStr => Number(numStr));
+  // check length of array
+  // if length === 1, return 1
+  if (numArray.length === 1) return 1;
+  // iterate from 0th el up to last el of numArray
+  for (let i = 0; i < numArray.length; i += 1) {
+    // if i equals numArray.length - 1; increment numOfTwos by 1 and return numOfTwos
+    if (i === numArray.length - 1) {
+      numOfTwos += 1;
+      return numOfTwos;
+    }
+    for (let x = 0; x < numArray[i]; x += 1) {
+      // on each iteration, iterate from 0 up to and including num, incrementing
+      // numOfTwos by 1 each time
+      numOfTwos += 1;
+    }
+  }
+  return numOfTwos;
 }
 
-module.exports = countTwos;
+console.log(countTwos(1)); // -> 0
+console.log(countTwos(3)); // -> 1
+console.log(countTwos(13)); // -> 2
+console.log(countTwos(1000)); // -> 300
+console.log(countTwos(11420)); // -> 4483
+
+// module.exports = countTwos;
