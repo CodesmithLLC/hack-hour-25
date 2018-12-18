44cb2e9efd72b3aa022fbaa1af7bfd460d901b8c
Jae Hyun Lee <jaehyunlee@Jaes-MacBook-Pro-2.local>
2018-12-18 09:32:56 -0800

    add ll

--- challenges/add-linked-list.js
+++ challenges/add-linked-list.js
@@ -18,7 +18,66 @@ function Node(val) {
 }
 
 function addLinkedList(l1, l2) {
+  // edge cases
+  if (l1 === null && l2 === null) return undefined;
+  if (l1 === null && l2 !== null) return l2;
+  if (l2 === null && l1 !== null) return l1;
 
+  let l1Val = '';
+  let l2Val = '';
+  let l1Curr = l1;
+  let l2Curr = l2;
+  let result = null;
+  
+  // loop through and get the 
+  while (l1Curr !== null || l2Curr !== null) {
+    if (l1Curr !== null) {
+      l1Val += l1Curr.value.toString();
+      l1Curr = l1Curr.next;
+    } 
+    if (l2Curr !== null) {
+      l2Val += l2Curr.value.toString();
+      l2Curr = l2Curr.next;
+    }
+  }
+
+  // get the sum and turn it into a reversed array
+  l1Val = l1Val.split('').reverse().join('') * 1;
+  l2Val = l2Val.split('').reverse().join('') * 1;
+  let sum = (l1Val + l2Val).toString().split('').reverse();
+
+  let current = null;
+  
+  // return the new linked list 
+  return sum.map(value => {
+    value = value * 1;
+    if (result === null) {
+      result = new Node(value);
+      current = result;
+    }
+    else {
+      const node = new Node(value);
+      current.next = node;
+      current = node;
+    }
+    return current;
+  });
 }
+
+// const one = new Node(2);
+// const two = new Node(1);
+// const hi = new Node(5);
+
+// one.next = two;
+// two.next = hi;
+
+// const three = new Node(5);
+// const four = new Node(9);
+// const h = new Node(0);
+
+// three.next = four;
+// four.next = h;
+
+// console.log(addLinkedList(one, three))
 
 module.exports = {Node: Node, addLinkedList: addLinkedList};
