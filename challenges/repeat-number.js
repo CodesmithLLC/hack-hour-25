88ab8a76b4430125226ab035d9d76c6e8031e711
codemccodeface <yurenfrankma@gmail.com>
2018-12-20 09:22:11 -0800

    finished repeat-num

--- challenges/repeat-number.js
+++ challenges/repeat-number.js
@@ -1,5 +1,5 @@
 /**
- * I have an array where every number in the range 1...n appears once except for 
+ * I have an array where every number in the range 1...n appears once except for
  * one number which appears twice.
  *
  * Write a function for finding the number that appears twice.
@@ -11,7 +11,19 @@
  */
 
 function repeatNumbers(array) {
-
+  const n = array.length - 1;
+  let sumN = 0;
+  for (let j = 1; j <= n; j += 1) {
+    sumN += j;
+  }
+  let sumArr = 0;
+  for (let i = 0; i < array.length; i += 1) {
+    sumArr += array[i];
+  }
+  return sumArr - sumN;
 }
+
+
+console.log('repeatNumbers: ', repeatNumbers([1, 2, 5, 4, 2, 6, 3]));
 
 module.exports = repeatNumbers;
