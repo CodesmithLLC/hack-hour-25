fc0a93625235361c6aef5027cd7a3cad14f24cf1
evgenii-codesmith <evgenii.codesmith@gmail.com>
2018-12-20 09:28:18 -0800

    repeat-number solved

--- challenges/repeat-number.js
+++ challenges/repeat-number.js
@@ -11,7 +11,19 @@
  */
 
 function repeatNumbers(array) {
+	let numOccur = {};
+	for (let i = 0; i < array.length;i++){
+		numOccur[array[i]] = numOccur[array[i]] + 1 || 1;
+	}
+	
+	
+	for (let key in numOccur){
+		if (numOccur[key] > 1) return key;
+	}
 
 }
 
+console.log(repeatNumbers([1,2,3,4]));
+console.log(repeatNumbers([1,2,4,3,4])); //4
+console.log(repeatNumbers([1,2,3,2,4,5,6])); //2
 module.exports = repeatNumbers;
