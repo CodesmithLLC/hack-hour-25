e06fc0838b5dd59a23ba8d080ed5d516f93976d7
Darryl Amour <darryl.amour@gmail.com>
2018-12-20 10:31:37 -0800

    complete repeat-number.js with O(n) solution.

--- challenges/repeat-number.js
+++ challenges/repeat-number.js
@@ -11,7 +11,13 @@
  */
 
 function repeatNumbers(array) {
-
+  let result = {};
+  for (let i = 0; i < array.length; i += 1) {
+    result[array[i]] = ((result[array[i]] += 1) || 1);
+    if (result[array[i]] > 1) {
+      return array[i];
+    }
+  }
 }
 
 module.exports = repeatNumbers;
