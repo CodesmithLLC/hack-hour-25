5e1292fa6effc74ca37bbeea3c5a81039424f042
Danni B <danni.ballena@gmail.com>
2018-12-20 09:36:40 -0800

    repeat-number

--- challenges/repeat-number.js
+++ challenges/repeat-number.js
@@ -9,9 +9,21 @@
  * Do this in O(1) space
  *
  */
-
+// [1, 2, 3, 4, 1, 5, 6];
 function repeatNumbers(array) {
-
+  const obj = {};
+  let result;
+  array.forEach(el => {
+    if (obj.hasOwnProperty(el)) {
+      result = el;
+    } else {
+      obj[el] = 0;
+    }
+  });
+  return result;
 }
+
+
+console.log(repeatNumbers([1, 2, 3, 1, 5, 6]));
 
 module.exports = repeatNumbers;
