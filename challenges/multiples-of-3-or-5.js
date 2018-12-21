e6067a65c488294d67f419e8f26d5fd9540c5b24
Dave Marquess <dave.marquess@gmail.com>
2018-12-21 09:41:35 -0800

    complete multiples-of-3-or-5

--- challenges/multiples-of-3-or-5.js
+++ challenges/multiples-of-3-or-5.js
@@ -7,16 +7,33 @@
 
 function sumMultiples3Or5Below1000() {
   let sum = 0;
-
+  // iterate from 3 up to 1000, incrementing by 3
+  for (let i = 3; i < 1000; i += 3) {
+    // add current num to sum
+    sum += i;
+  }
+  // iterate from 5 up to 1000, incrementing by 5
+  for (let i = 5; i < 1000; i += 5) {
+    // add current num to sum
+    sum += i;
+  }
   return sum;
 }
-
 
 // extension make it dynamic function that takes input x,y,z
 // and returns the sum of multiples of x and y below z
 function sumMultiplesXOrYBelowZ(x, y, z) {
   let sum = 0;
-
+  // iterate from x up to z, incrementing by x
+  for (let i = x; i < z; i += x) {
+    // add current num to sum
+    sum += i;
+  }
+  // iterate from y up to z, incrementing by y
+  for (let i = y; i < z; i += y) {
+    // add current num to sum
+    sum += i;
+  }
   return sum;
 }
 
