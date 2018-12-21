e15ef367260f3c5646811cf8b5f7a0992da15d56
Hei Yeung Lam <heiyeunglam@Heis-MacBook-Pro.local>
2018-12-21 09:30:39 -0800

    yum yum yum yum

--- challenges/multiples-of-3-or-5.js
+++ challenges/multiples-of-3-or-5.js
@@ -8,6 +8,11 @@
 function sumMultiples3Or5Below1000() {
   let sum = 0;
 
+  for (let i = 3; i < 1000; i += 1) {
+  	if (i % 3 === 0 || i % 5 === 0) {
+  	  sum += i;
+  	}
+  }
   return sum;
 }
 
@@ -15,8 +20,18 @@ function sumMultiples3Or5Below1000() {
 // extension make it dynamic function that takes input x,y,z
 // and returns the sum of multiples of x and y below z
 function sumMultiplesXOrYBelowZ(x, y, z) {
-  let sum = 0;
+  let sum = 0
+  if (x > y) {
+  	let temp = y;
+  	y = x
+  	x = temp
+  }
 
+  for (let i = x; i < z; i += 1) {
+  	if (i % x === 0 || i % y === 0) {
+  	  sum += i
+  	}
+  }
   return sum;
 }
 
