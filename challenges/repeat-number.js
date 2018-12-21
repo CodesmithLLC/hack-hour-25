13da92ce6c198d89fdeea9d70ad50d4099133b20
Timothy Kachler <kachler@mac.com>
2018-12-21 09:26:02 -0800

    completed repeat-number challenge

--- challenges/repeat-number.js
+++ challenges/repeat-number.js
@@ -10,8 +10,25 @@
  *
  */
 
-function repeatNumbers(array) {
+// function repeatNumbers(array) {
+//   const countObj = {};
+//   for (let i = 0; i < array.length; i += 1) {
+//     if (!countObj[array[i]]) countObj[array[i]] = 1;
+//     else return array[i];
+//   }
+// }
 
+function repeatNumbers(array) {
+  let { length } = array;
+  while (length >= 0) {
+    if (array.lastIndexOf(array[0]) !== 0) return array[0];
+    array.shift();
+    length -= 1;
+  }
+  return -1;
 }
+
+const numArray = [1, 6, 3, 4, 4, 5, 12];
+console.log(repeatNumbers(numArray));
 
 module.exports = repeatNumbers;
