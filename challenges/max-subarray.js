aa5a31320b3379a3fb2c14d40c8622845a578024
Jae Hyun Lee <jaehyunlee@Jaes-MacBook-Pro-2.local>
2018-12-22 09:36:40 -0800

    max subarray

--- challenges/max-subarray.js
+++ challenges/max-subarray.js
@@ -7,8 +7,27 @@
  *
  */
 
-function maxSubarray(arr) {
+function maxSubarray(arr, max = 0, first = true) {
+	// base case
+	if (arr.length === 0) return max;
 
+	let copyArr = arr;
+	// only copy the array once and manipulate that array
+	if (first) copyArr = arr.slice();
+
+	// reduce array to find some, and during the process, set max if found
+	const sum = copyArr.reduce((acc, cur) => {
+		if (acc > max) max = acc;
+		return acc + cur;
+	});
+
+	// check the final sum to see if it the new max
+	if (sum > max) max = sum;
+
+	// remove first element of copyArr to pass to next recursive call
+	copyArr.shift();
+
+	return maxSubarray(copyArr, max, false);
 }
 
 module.exports = maxSubarray;
