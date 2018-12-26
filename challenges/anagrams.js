20608545b463d2efd0022f49fdf96654c82d3aa2
Jae Hyun Lee <jaehyunlee@Jaes-MacBook-Pro-2.local>
2018-12-26 09:39:34 -0800

    anagrams

--- challenges/anagrams.js
+++ challenges/anagrams.js
@@ -12,8 +12,32 @@
   * console.log(result); // [ 'abc', 'acb', 'bac', 'bca', 'cab', 'cba' ]
   */
 
-function anagrams(string) {
+function anagrams(string, result = []) {
+  if (string.length === 0) return [];
+  if (string.length === 1) return [string];
+  if (string.length === 2) return [string[0] + string[1], string[1] + string[0]];
+  
+  const checker = {};
 
+  for (let i = 0; i < string.length; i += 1) {
+    let copyStr = '';
+    if (i < string.length - 1) copyStr = string.substring(0, i) + string.substring(i + 1);
+    else copyStr = string.substring(0, i);
+
+    // recursively call anagrams of the string without the current letter in string
+    const eachAnagram = anagrams(copyStr);
+
+    // attach the current letter in string to each result
+    const eachResult = eachAnagram.map(res => string[i] + res);
+
+    // check if entry already exists
+    eachResult.map(el => {
+      if (!checker[el]) result.push(el);
+      else checker[el] = el;
+      return;
+    })
+  }
+  return result;
 }
 
 module.exports = anagrams;
