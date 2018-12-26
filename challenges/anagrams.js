a2eb83824d83003c4645aadf31d8348af0d3779f
Jim Yoon <jimyoon90@gmail.com>
2018-12-26 10:00:55 -0800

    unfinished anagram

--- challenges/anagrams.js
+++ challenges/anagrams.js
@@ -13,7 +13,32 @@
   */
 
 function anagrams(string) {
-
+  // have an array to keep track of all possible outcomes
+  const results = [];
+  // split the string and store in variable
+  const original = string.split('');
+  // checks for 1 or 2 length string
+  if (original.length === 1) return original;
+  if (original.length === 2) return original.concat(original.reverse());
+  // have a function that produces all perm of the string
+  function anaPerm(arr) {
+    // base cases
+    if (arr.length === 1) return arr;
+    if (arr.length === 2) return arr.concat(arr.reverse());
+    // recusive logic
+    arr.forEach((el, index, array) => {
+      const newArr = [...array];
+      newArr.splice(index, 1);
+      anaPerm(newArr).forEach((elem) => {
+        // console.log(elem);
+        results.push(el + elem);
+      });
+    });
+  }
+  anaPerm(original);
+  return results;
 }
+
+console.log(anagrams('abc'));
 
 module.exports = anagrams;
