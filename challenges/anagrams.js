bcd75be0d6111de560a6e06ab2c077101a42b2c2
codemccodeface <yurenfrankma@gmail.com>
2018-12-26 22:02:22 -0800

    finished anagrams

--- challenges/anagrams.js
+++ challenges/anagrams.js
@@ -13,7 +13,18 @@
   */
 
 function anagrams(string) {
-
+  const result = [];
+  if (string.length === 1) return [string];
+  const curr = string.slice(0, 1);
+  const rest = string.slice(1);
+  anagrams(rest).forEach((str) => {
+    for (let i = 0; i <= str.length; i += 1) {
+      const newStr = str.slice(0, i) + curr + str.slice(i);
+      result.push(newStr);
+    }
+  });
+  return result;
 }
+
 
 module.exports = anagrams;
