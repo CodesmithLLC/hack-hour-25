8f73f9a90dc177c15aa3dcb78fcb328325a49f03
Darryl Amour <darryl.amour@gmail.com>
2018-12-27 09:49:34 -0800

    completed anagrams.js challenge

--- challenges/anagrams.js
+++ challenges/anagrams.js
@@ -12,8 +12,43 @@
   * console.log(result); // [ 'abc', 'acb', 'bac', 'bca', 'cab', 'cba' ]
   */
 
-function anagrams(string) {
+function anagrams(string, pointer = 0, rotations = 0, results = []) {
+  // Edge Case: string provided is empty, null or undefined
+  if (string === '' || string === null || string === undefined) return results;
 
+  // Base Case: Exit when each character of string array has been processed
+  if (pointer === string.length) return results;
+
+  if (rotations === string.length) return results;
+
+  // Create anagram for each character in string array via string rotation
+  results.concat(anagrams(
+    string.substr(0, 1) + rotate(string.substr(1, string.length - 1)),
+    pointer,
+    rotations += 1,
+    results,
+  ));
+ 
+  results.concat(anagrams(
+    rotate(string),
+    pointer += 1,
+    rotations,
+    results,
+  ));
+
+  // add string to result
+  results.push(string);
+
+  console.log(`pointer: ${pointer}, rotation: ${rotations}, results: ${results}, length: ${results.length}`);
+
+  return Array.from((new Set(results)).values());
 }
+
+const rotate = (string) => {
+  const stringArray = string.split('');
+  const firstElement = stringArray.shift();
+  stringArray.push(firstElement);
+  return stringArray.join('');
+};
 
 module.exports = anagrams;
