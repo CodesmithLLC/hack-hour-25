a1368e6782b58164f8defc048394b6370a15f7e2
Danni B <danni.ballena@gmail.com>
2018-12-27 10:17:10 -0800

    delete-dups

--- challenges/delete-dups.js
+++ challenges/delete-dups.js
@@ -10,10 +10,27 @@
  * How would you solve this problem if a temporary buffer is not allowed?
  */
 
-
+function Node(val) {
+  this.value = val;
+  this.next = null;
+}
 
 function deleteDups(head) {
-
+  let curr = head;
+  let prev = null;
+  const obj = {};
+  if (curr.next === null) return curr;
+  while (curr) {
+    if (!obj.hasOwnProperty(curr.value)) {
+      obj[curr.value] = 0;
+      prev = curr;
+      curr = curr.next;
+    } else {
+      prev.next = curr.next;
+      curr = curr.next;
+    }
+  }
+  return curr;
 }
 
 module.exports = deleteDups;
