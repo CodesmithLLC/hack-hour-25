c46760e4a7ab1e076adf6b770180bb2643a2f22a
Hei Yeung Lam <heiyeunglam@Heis-MacBook-Pro.local>
2018-12-28 09:34:59 -0800

    delete-dups

--- challenges/delete-dups.js
+++ challenges/delete-dups.js
@@ -13,7 +13,26 @@
 
 
 function deleteDups(head) {
+	let cache = {}
+	cache[head.value] = 1
+	pointer = head.next
+	second_pointer = head
+	while (pointer !== null) {
+        console.log(cache, "pointer value: " + pointer.value)
+    if (pointer.next !== null && cache.hasOwnProperty
+    (pointer.value)) {
+      console.log(cache)
+				second_pointer.next = pointer.next;
+			} else if (pointer.next === null && cache.hasOwnProperty
+    (pointer.value)) {
+        console.log('hi')
+        second_pointer.next = null;}
+		cache[pointer.value] = 1;
 
+		pointer = pointer.next;
+		second_pointer = second_pointer.next;
+  }
+	return head
 }
 
 module.exports = deleteDups;
