a4244a3eaf82141fe95dcb21f3cc7c48379eaf0a
Darryl Amour <darryl.amour@gmail.com>
2018-12-28 09:43:46 -0800

    completed event-emitter.js and tested

--- challenges/event-emitter.js
+++ challenges/event-emitter.js
@@ -22,15 +22,27 @@
  */
 
 function EventEmitter() {
-
+  this.events = {};    
 }
 
 EventEmitter.prototype.on = function(funcName, func) {
-
+  // Add event with callback to array
+  // Goal is for this.events to look like {funcName: [func,...]}
+  if (this.events.hasOwnProperty(funcName)) {
+    this.events[funcName].push(func);
+    console.log(`this.events: ${this.events}`);
+  } else {
+    this.events[funcName] = [func];
+  }
 };
 
 EventEmitter.prototype.trigger = function(funcName, ...args) {
-
+  // if event has been defined, cycle thru each of it's call backs
+  if(this.events.hasOwnProperty(funcName)) {
+    for (let i = 0; i < this.events[funcName].length; i += 1) {
+      this.events[funcName][i](...args);
+    }
+  }
 };
 
 module.exports = EventEmitter;
