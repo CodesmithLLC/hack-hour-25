79ec21b6dd76259c56538369e7272ee0b6412ea1
Timothy Kachler <kachler@mac.com>
2018-12-28 09:47:04 -0800

    completed event-emitter challenge

--- challenges/event-emitter.js
+++ challenges/event-emitter.js
@@ -1,3 +1,4 @@
+/* eslint-disable func-names */
 'use strict';
 /**
  * Make an EventEmitter that
@@ -21,16 +22,47 @@
  * - It is not necessary to write a way to remove listeners.
  */
 
-function EventEmitter() {
 
+function EventEmitter(func) {
+  this.events = {};
 }
 
-EventEmitter.prototype.on = function(funcName, func) {
-
+EventEmitter.prototype.on = function (funcName, func) {
+  if (!this.events[funcName]) this.events[funcName] = [func];
+  else this.events[funcName].push(func);
 };
 
-EventEmitter.prototype.trigger = function(funcName, ...args) {
-
+EventEmitter.prototype.trigger = function (funcName, ...args) {
+  this.events[funcName].forEach((event) => {
+    event();
+  });
+  if (args) {
+    args.forEach((event) => {
+      this.events[event].forEach((func) => {
+        func();
+      });
+    });
+  }
 };
+
+const instance = new EventEmitter();
+let counter = 0;
+instance.on('increment', () => {
+  counter += 1;
+});
+
+instance.on('decrement', () => {
+  counter -= 1;
+});
+
+console.log(instance.events);
+instance.trigger('increment');
+console.log('Counter: ', counter); // counter should be 1
+instance.trigger('increment');
+console.log('Counter: ', counter); // counter should be 2
+instance.trigger('decrement');
+console.log('Counter: ', counter); // counter should be 1
+instance.trigger('increment', 'decrement');
+console.log('Counter: ', counter); // counter should be 2
 
 module.exports = EventEmitter;
