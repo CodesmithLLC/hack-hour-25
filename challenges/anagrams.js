1c01c6d9ef36dabae4980cd3e86186938534db6c
Tony Hudgins <tonyhudgins@gmail.com>
2018-12-28 09:23:30 -0800

    completed anagrams challenge

--- challenges/anagrams.js
+++ challenges/anagrams.js
@@ -12,8 +12,18 @@
   * console.log(result); // [ 'abc', 'acb', 'bac', 'bca', 'cab', 'cba' ]
   */
 
-function anagrams(string) {
-
+ function anagrams(string) {
+  const anagramsArr = {};
+  if (string.length === 1) return [string]
+  for (let i = 0; i < string.length; i++){
+    let char = string[0];
+    let subs = anagrams(string.substring(1))
+    for (let j = 0; j < subs.length; j ++) {
+      anagramsArr[char.concat(subs[j])]=1
+    }
+    string = string.substring(1).concat(string[0])
+  }
+  return (Object.keys(anagramsArr))    
 }
 
 module.exports = anagrams;
