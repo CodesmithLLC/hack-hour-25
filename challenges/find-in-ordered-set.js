eb22163997255bcc546b6105b293db185993d729
Jim Yoon <jimyoon90@gmail.com>
2018-12-29 09:20:30 -0800

    finished find-in-ordered-set

--- challenges/find-in-ordered-set.js
+++ challenges/find-in-ordered-set.js
@@ -9,10 +9,29 @@ findInOrderedSet(nums, 2);  -> false
 
  */
 
+const nums = [1, 4, 6, 7, 9, 17, 45];
 
+// // 1 loop, O(n)
+// function findInOrderedSet(arr, target) {
+//   return arr.includes(target);
+// }
+
+
+// faster
 function findInOrderedSet(arr, target) {
-
+  let track = 0;
+  while (track < arr.length) {
+    if (arr[track] > target) {
+      return false;
+    }
+    if (arr[track] === target) {
+      return true;
+    }
+    track += 1;
+  }
 }
 
+console.log(findInOrderedSet(nums, 4));
+console.log(findInOrderedSet(nums, 2));
 
 module.exports = findInOrderedSet;
