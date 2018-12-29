fa00c5ab5382fe5c130729f18ecec869c78b493a
codemccodeface <yurenfrankma@gmail.com>
2018-12-29 09:20:01 -0800

    finished findinorderedset

--- challenges/find-in-ordered-set.js
+++ challenges/find-in-ordered-set.js
@@ -11,8 +11,16 @@ findInOrderedSet(nums, 2);  -> false
 
 
 function findInOrderedSet(arr, target) {
-
+  if (arr.length === 1) return target === arr[0];
+  const mid = Math.floor(arr.length / 2);
+  if (target === arr[mid]) return true;
+  if (target < arr[mid]) return findInOrderedSet(arr.slice(0, mid), target);
+  return findInOrderedSet(arr.slice(mid), target);
 }
+
+// var nums = [1, 4, 6, 7, 9, 17, 45]
+// console.log(findInOrderedSet(nums, 4));  
+// console.log(findInOrderedSet(nums, 2));  
 
 
 module.exports = findInOrderedSet;
