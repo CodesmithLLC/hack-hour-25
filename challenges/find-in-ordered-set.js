f7535e3b030a924773beca1b8e2c14a47dd11259
Jae Hyun Lee <jaehyunlee@Jaes-MacBook-Pro-2.local>
2018-12-29 09:31:47 -0800

    find in ordered set

--- challenges/find-in-ordered-set.js
+++ challenges/find-in-ordered-set.js
@@ -11,7 +11,19 @@ findInOrderedSet(nums, 2);  -> false
 
 
 function findInOrderedSet(arr, target) {
+	// edge cases
+	if (arr.length === 0) return false;
+	if (isNaN(target)) return false;
+	
+	if (arr.length === 1 && arr[0] === target) return true;
+	if (arr.length === 1 && arr[0] !== target) return false;
 
+	const middle = Math.floor(arr.length / 2);
+	if (target === arr[middle]) return true;
+	if (target > arr[middle]) return findInOrderedSet(arr.slice(middle, arr.length + 1), target);
+	if (target < arr[middle]) return findInOrderedSet(arr.slice(0, middle), target);
+
+	return false;
 }
 
 
