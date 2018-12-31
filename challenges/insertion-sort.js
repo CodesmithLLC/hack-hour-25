75c22befff99471b2a7c17e665ae199831874aac
codemccodeface <yurenfrankma@gmail.com>
2018-12-31 10:12:42 -0800

    finished insertsort

--- challenges/insertion-sort.js
+++ challenges/insertion-sort.js
@@ -3,9 +3,21 @@
 // https://en.wikipedia.org/wiki/Insertion_sort
 
 
-
 function insertionSort(array) {
-
+  for (let i = 1; i < array.length; i += 1) {
+    const key = arr[i];
+    let j = i - 1;
+    while (j >= 0 && key < arr[j]) {
+      arr[j + 1] = arr[j];
+      j -= 1;
+      arr[j + 1] = key;
+    }
+  }
 }
+
+let arr = [1, 3, 2];
+insertionSort(arr);
+console.log('arr: ', arr);
+
 
 module.exports = insertionSort;
