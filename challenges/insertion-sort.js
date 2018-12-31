34ffeb9041ec29350801ec08b57434d1de0f1055
Darryl Amour <darryl.amour@gmail.com>
2018-12-31 11:46:20 -0800

    completed insertion-sort.js w/ jest testing

--- challenges/insertion-sort.js
+++ challenges/insertion-sort.js
@@ -5,7 +5,22 @@
 
 
 function insertionSort(array) {
-
+  // console.log(array);
+  // iterate thru the array starting at element 1
+  for (let i = 1; i < array.length; i += 1) {
+    // console.log(`Evaluating: ${array[i]} @ pos: ${i}`);
+    // Iterate backward until until in right position
+    for (let j = i; j > 0; j -= 1) {
+      // console.log(`Comparing: ${array[j]} against: ${array[j - 1]}`)
+      // if current element is less than previous element swap
+      if (array[j] < array[j - 1]) {
+        const temp = array[j];
+        array[j] = array[j - 1];
+        array[j - 1] = temp;
+      } else break;
+    }
+  }
+  // console.log(array);
 }
 
 module.exports = insertionSort;
