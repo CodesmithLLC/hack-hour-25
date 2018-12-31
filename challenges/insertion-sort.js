452f0e255f20f2d44333f9e60c45f1c4fe9357cd
Josie Glore <josieglore@gmail.com>
2018-12-31 09:29:17 -0800

    add insertion sort

--- challenges/insertion-sort.js
+++ challenges/insertion-sort.js
@@ -5,7 +5,17 @@
 
 
 function insertionSort(array) {
-
+  for (let i = 0; i < array.length; i += 1) {
+    for (let j = i + 1; j < array.length; j += 1) {
+      if (array[i] > array[j]) {
+        const temp = array[i];
+        array[i] = array[j];
+        array[j] = temp;
+      }
+    }
+  }
+  return array;
 }
 
+console.log(insertionSort([2, 1, 4, 3, 6, 9, 8]))
 module.exports = insertionSort;
