697a63e80f66cd72317452bee702ae3f2744f2dd
Dave Marquess <dave.marquess@gmail.com>
2018-12-31 09:44:54 -0800

    complete insertion-sort

--- challenges/insertion-sort.js
+++ challenges/insertion-sort.js
@@ -5,7 +5,17 @@
 
 
 function insertionSort(array) {
+  if (array.length === 0) return array;
+  const arrCopy = [...array];
 
+  for (let i = 0; i < array.length; i += 1) {
+    for (let x = array.length; x >= 0; x -= 1) {
+      if (arrCopy[x] < arrCopy[x - 1]) {
+        [arrCopy[x], arrCopy[x - 1]] = [arrCopy[x - 1], arrCopy[x]];
+      }
+    }
+  }
+  return arrCopy;
 }
 
 module.exports = insertionSort;
