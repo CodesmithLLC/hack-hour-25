63ef23c7e43627e48ca4da7f05a2b4e87b71d113
Jason Wong <jasonwong@Jasons-MacBook-Pro.local>
2018-12-31 09:46:13 -0800

    insertionsort

--- challenges/insertion-sort.js
+++ challenges/insertion-sort.js
@@ -5,7 +5,16 @@
 
 
 function insertionSort(array) {
-
+  for (let i = 1; i < array.length; i += 1) {
+    for (let j = i; j >= 0; j -= 1) {
+      if (array[j] < array[j - 1]) {
+        const holder = array[j];
+        array[j] = array[j - 1];
+        array[j - 1] = holder;
+      }
+    }
+  }
+  return array;
 }
 
 module.exports = insertionSort;
