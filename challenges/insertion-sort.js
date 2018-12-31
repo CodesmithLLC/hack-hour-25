1eb79a36b5393ace3a675dce7e4ae8a52d6bfe48
Timothy Kachler <kachler@mac.com>
2018-12-31 09:46:13 -0800

    committing insertion-sort challenge

--- challenges/insertion-sort.js
+++ challenges/insertion-sort.js
@@ -14,6 +14,22 @@ function insertionSort(array) {
   return array;
 }
 
+//SOLUTION 1
+// Worst case : O(n^2)
+// Best case: already sorted O(n)
+
+// function insertionSort(array) {
+//   for (var i = 1; i < array.length; i++) {
+//     var j = i;
+//     while (array[j] < array[j-1] && j > 0) {
+//       var temp = array[j];
+//       array[j] = array[j-1];
+//       array[--j] = temp;
+//     }
+//   }
+//   return array;
+// }
+
 const testArray = [10, 13, 4, 5, 17];
 console.log(insertionSort(testArray));
 
