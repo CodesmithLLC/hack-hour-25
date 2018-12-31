d522cd9419f6e5a234a45c8f5b4fcac36cb9c9e3
Timothy Kachler <kachler@mac.com>
2018-12-31 09:37:39 -0800

    committing insertion-sort challenge

--- challenges/insertion-sort.js
+++ challenges/insertion-sort.js
@@ -2,10 +2,19 @@
 
 // https://en.wikipedia.org/wiki/Insertion_sort
 
-
-
 function insertionSort(array) {
-
+  for (let i = 1; i < array.length; i += 1) {
+    for (let j = 0; j < i; j += 1) {
+      if (array[i] < array[j]) {
+        const temp = array.splice(i, 1);
+        array.splice(j, 0, temp[0]);
+      }
+    }
+  }
+  return array;
 }
+
+const testArray = [10, 13, 4, 5, 17];
+console.log(insertionSort(testArray));
 
 module.exports = insertionSort;
