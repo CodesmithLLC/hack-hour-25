7292366bc554d7c5186f9e2003022b146f5afe56
Timothy Kachler <kachler@mac.com>
2019-01-02 10:40:01 -0800

    completed unique-number challenge

--- challenges/unique-number.js
+++ challenges/unique-number.js
@@ -9,8 +9,29 @@
  * Complete the challenge in O(1) space
  *
  */
-function uniqueNumber(array) {
+// function uniqueNumber(array) {
+//   const output = {};
+//   for (let i = 0; i < array.length; i += 1) {
+//     if (!output[array[i]]) output[array[i]] = array[i];
+//     else delete output[array[i]];
+//   }
+//   console.log(output);
+//   return Object.keys(output)[0];
+// }
 
+function uniqueNumber(array) {
+  // eslint-disable-next-line prefer-destructuring
+  const { length } = array;
+  let res = array[0];
+  for (let i = 1; i < length; i += 1) {
+    console.log(res, array[i]);
+    console.log(res ^ array[i]);
+    res = res ^ array[i]; 
+  }
+  return res;
 }
+
+
+console.log(uniqueNumber([1, 2, 1, 3, 3])); // Returns 2
 
 module.exports = uniqueNumber;
