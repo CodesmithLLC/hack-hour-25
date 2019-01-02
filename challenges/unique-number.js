13b03c037c516218cc8875255ed1362d93311916
Tony Hudgins <tonyhudgins@gmail.com>
2019-01-02 10:13:35 -0800

    completed unique number

--- challenges/unique-number.js
+++ challenges/unique-number.js
@@ -10,7 +10,11 @@
  *
  */
 function uniqueNumber(array) {
-
+  let accum = array[0]
+  for (let i = 1; i < array.length; i++){
+    accum = accum ^ array[i]
+  }
+  return accum
 }
 
 module.exports = uniqueNumber;
