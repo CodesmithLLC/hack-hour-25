56f482db0f4daebc1a7e4f5a9d585d800d32d471
Dave Marquess <dave.marquess@gmail.com>
2019-01-02 09:56:34 -0800

    complete unique-number

--- challenges/unique-number.js
+++ challenges/unique-number.js
@@ -10,7 +10,22 @@
  *
  */
 function uniqueNumber(array) {
-
+  if (array.length === 0) return false;
+  // create object to store nums as keys and number of times occured as values
+  const pairsObj = {};
+  // iterate thru array
+  for (let i = 0; i < array.length; i += 1) {
+    // if num exists, increment it's corresponding value
+    if (pairsObj[array[i]]) pairsObj[array[i]] += 1;
+    // otherwise, set num as key and val equal to 1
+    if (!pairsObj[array[i]]) pairsObj[array[i]] = 1;
+  }
+  // iterate thru object's keys and return key where val is equal to 1
+  const keys = Object.keys(pairsObj);
+  for (let i = 0; i < keys.length; i += 1) {
+    if (pairsObj[keys[i]] === 1) return keys[i];
+  }
+  return false;
 }
 
 module.exports = uniqueNumber;
