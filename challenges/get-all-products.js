76aa9ff80d2aefe38e5a499e7a1aeb1a53be8034
Josie Glore <josieglore@gmail.com>
2019-01-03 09:43:43 -0800

    fix all possible products

--- challenges/get-all-products.js
+++ challenges/get-all-products.js
@@ -10,15 +10,21 @@
  */
 
 function getAllProducts(array) {
+  if (array.length === 0) return 0;
   let products = [];
   for (let i = 0; i < array.length; i += 1) {
     let product = 1;
     for (let j = 0; j < array.length; j += 1) {
       if (j !== i) product *= array[j];
     }
-    if (!products.includes(product)) products.push(product);
+    products.push(product);
   }
   return products;
 }
+console.log(getAllProducts([1, 7, 3, 4])); // Should return [84, 12, 28, 21]
+console.log(getAllProducts([0, 1, 3, 4])); // Should return [ 12, 0, 0, 0 ]
+console.log(getAllProducts([1, 1, 1, 4])); // Should return [ 4, 4, 4, 1 ]
+console.log(getAllProducts([0, 0, 0])); // Should return [ 0, 0, 0 ]
+console.log(getAllProducts([0, 1])); // Should return [ 1, 0 ]
 
 module.exports = getAllProducts;
