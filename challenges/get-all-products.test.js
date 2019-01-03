b2c6d5991221000e29a299c9b6fcb9489e603afd
Darryl Amour <darryl.amour@gmail.com>
2019-01-03 10:04:21 -0800

    Adjusted to handle empty array w/ proper jest testing added.

--- challenges/get-all-products.test.js
+++ challenges/get-all-products.test.js
@@ -3,3 +3,8 @@ const getAllProducts = require('./get-all-products');
 test('getAllProducts Test #1', () => {
   expect(getAllProducts([1, 7, 3, 4])).toEqual([84, 12, 28, 21]);
 });
+
+test('getAllProducts Test #2', () => {
+  expect(getAllProducts([])).toEqual([0]);
+});
+
