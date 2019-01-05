dad60d62a0c4223d65342e1224df72cf2011c0a5
vwang4536 <vwang4536@gmail.com>
2019-01-05 10:21:21 -0800

    complete doubly linkedlist

--- challenges/doubly-linked-list.js
+++ challenges/doubly-linked-list.js
@@ -17,14 +17,31 @@ function Node(val) {
 Adds a node to the end of the list
  */
 LinkedList.prototype.add = function(val) {
-  
+  let newNode = new Node(val);
+  let current = this.head;
+  while (current.next) {
+    current = current.next
+  };
+  current.next = newNode;
+  newNode.prev = current;
+  this.tail = newNode
 };
 
 /*
 Removes the first node with the inputted value
  */
 LinkedList.prototype.remove = function(val) {
-  
-};
+  if (this.head.val === val) {this.head = this.head.next; return}
+  let prev = this.head;
+  let current = prev.next;
+  while (current.val) {
+    if (current.val === val) {
+      this.prev.next = current.next;
+      return;
+    };
+    prev = prev.next;
+    current = current.next;
+  }
+}; 
 
 module.exports = LinkedList;
