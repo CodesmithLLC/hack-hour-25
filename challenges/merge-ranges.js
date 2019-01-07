13fc58aac92614a7f71696b11ca779d9c174d6ca
Jason Wong <jwaosnogn@gmail.com>
2019-01-07 10:35:06 -0800

    dont know how to delete dups

--- challenges/merge-ranges.js
+++ challenges/merge-ranges.js
@@ -11,7 +11,24 @@
 
 
 function mergeRanges(array) {
-
+  // for every element in array, compare both digits in item to every digit in every other item in array
+  // either either digit is within the range other elements, push min and max of all digits of those two elements
+  const result = [];
+  for (let i = 0; i < array.length; i += 1) {
+    let check = true;
+    for (let j = 0; j < array.length; j += 1) {
+      if (i !== j && ((array[i][0] >= array[j][0] && array[i][0] <= array[j][1]) || (array[i][1] >= array[j][0] && array[i][1] <= array[j][1]))) {
+        const min = Math.min(...array[i], ...array[j]);
+        const max = Math.max(...array[i], ...array[j]);
+        result.push([min, max]);
+        check = false;
+      }
+    }
+    if (check) {
+      result.push(array[i]);
+    }
+  }
+  return result;
 }
 
 module.exports = mergeRanges;
