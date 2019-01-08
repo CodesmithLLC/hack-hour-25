1f49563be0b5e5ce6171290ee983f4f42b8c516a
codemccodeface <yurenfrankma@gmail.com>
2019-01-08 09:13:48 -0800

    finished bubblesort

--- challenges/bubble-sort.js
+++ challenges/bubble-sort.js
@@ -5,7 +5,19 @@
 
 
 function bubbleSort(array) {
-
+  let sorted = true;
+  for (let i = 0; i < array.length - 1; i += 1) {
+    if (array[i] > array[i + 1]) {
+      sorted = false;
+      const curr = array[i];
+      array[i] = array[i + 1];
+      array[i + 1] = curr;
+    }
+  }
+  if (!sorted) return bubbleSort(array);
+  return array;
 }
+
+console.log(bubbleSort([2, 1]));
 
 module.exports = bubbleSort;
