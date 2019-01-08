3146739fcb62624ca34a7d0f5deced75aa4a1a43
Dave Marquess <dave.marquess@gmail.com>
2019-01-08 09:34:11 -0800

    complete bubble-sort

--- challenges/bubble-sort.js
+++ challenges/bubble-sort.js
@@ -5,7 +5,18 @@
 
 
 function bubbleSort(array) {
-
+  for (let x = 0; x < array.length; x += 1) {
+    // iterate thru array
+    for (let i = 0; i < array.length; i += 1) {
+      // check if current el is greater than next el, if so swap els
+      if (array[i] > array[i + 1]) {
+        let tempEl = array[i];
+        array[i] = array[i + 1];
+        array[i + 1] = tempEl;
+      }
+    }
+  }
+  return array;
 }
 
 module.exports = bubbleSort;
