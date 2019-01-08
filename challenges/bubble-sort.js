d1af3a0852a39ab507e4f80286492b4e238776c1
jaelee213 <jaelee213@gmail.com>
2019-01-08 09:33:33 -0800

    bubble sort

--- challenges/bubble-sort.js
+++ challenges/bubble-sort.js
@@ -5,7 +5,17 @@
 
 
 function bubbleSort(array) {
-
+	if (!array || array.length === 0 || array.length === 1) return array;
+	let sorted = false;
+	for (let i = 0; i < array.length - 1; i += 1) {
+		if (array[i] > array[i + 1]) {
+			const hold = array[i];
+			array[i] = array[i + 1];
+			array[i + 1] = hold;
+			sorted = true;
+		}
+	}
+	return (sorted) ? bubbleSort(array) : array;
 }
 
 module.exports = bubbleSort;
--- challenges/merge-ranges.js
+++ challenges/merge-ranges.js
@@ -11,7 +11,23 @@
 
 
 function mergeRanges(array) {
+	if (array.length === 0) return [];
+	if (array.length === 1) return array;
 
+	let min = Number.POSITIVE_INFINITY;
+ 	let max = Number.NEGATIVE_INFINITY;
+	const result = [];
+
+	for (arr of array) {
+		const [from, to] = arr;
+		if (result.length === 0) result.push(arr);
+		else result.forEach(el => {
+			const [f, t] = el;
+			if (to > from && to < t) result.push([from, t]);
+		})
+	}
 }
 
 module.exports = mergeRanges;
+
+mergeRanges([[0, 1], [3, 5], [4, 8], [10, 12], [9, 10]])
