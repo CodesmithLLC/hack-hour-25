99b51c0648406f854b9415e5bec6d651e7bb9087
Jason Wong <jwaosnogn@gmail.com>
2019-01-08 09:36:23 -0800

    bubblesort

--- challenges/bubble-sort.js
+++ challenges/bubble-sort.js
@@ -5,7 +5,19 @@
 
 
 function bubbleSort(array) {
-
+  let switched = true;
+  while (switched) {
+    switched = false;
+    for (let i = 0; i < array.length - 1; i += 1) {
+      if (array[i] > array[i + 1]) {
+        switched = true;
+        const holder = array[i];
+        array[i] = array[i + 1];
+        array[i + 1] = holder;
+      }
+    }
+  }
+  return array;
 }
 
 module.exports = bubbleSort;
