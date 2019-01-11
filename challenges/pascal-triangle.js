9f2002d3b0cd08cd9fcf7b5237bacfc6324ab585
Jim Yoon <jimyoon90@gmail.com>
2019-01-11 10:16:50 -0800

    finished pascals triangle

--- challenges/pascal-triangle.js
+++ challenges/pascal-triangle.js
@@ -33,7 +33,39 @@
 */
 
 function pascalTriangle(numRows) {
-
+  // push all arrays into returnArr
+  const returnArr = [];
+  // loop to n
+  for (let i = 1; i <= numRows; i += 1) {
+    // inner array that will push into returnArr
+    const innerArr = [];
+    // 1 and 2 length will be put in
+    if (i === 1) {
+      innerArr.push(1);
+      returnArr.push(innerArr);
+    }
+    if (i === 2) {
+      innerArr.push(1, 1);
+      returnArr.push(innerArr);
+    }
+    // after 2, we can do array operations for pascals
+    if (i > 2) {
+      // push in the first and last number
+      innerArr.push(returnArr[i - 2][0]);
+      // have a for loop to go through the last innerArr and add the numbers up, then push into innerArr
+      for (let j = 1; j < returnArr[i - 2].length; j += 1) {
+        innerArr.push(returnArr[i - 2][j] + returnArr[i - 2][j - 1]);
+      }
+      // add in last number
+      innerArr.push(returnArr[i - 2][returnArr.length - 1]);
+      // after looping is done, push into returnArr
+      returnArr.push(innerArr);
+    }
+  }
+  // return the tree
+  return returnArr;
 }
+
+console.log(pascalTriangle(6));
 
 module.exports = pascalTriangle;
