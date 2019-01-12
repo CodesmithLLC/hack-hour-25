79253516df58eb7c9c24042a3bb1dfe947290c28
codemccodeface <yurenfrankma@gmail.com>
2019-01-12 09:38:51 -0800

    finished observed

--- challenges/observed-pin.js
+++ challenges/observed-pin.js
@@ -1,7 +1,7 @@
 /*
-Alright, detective, one of our colleagues successfully observed our target person, Robby the robber. 
-We followed him to a secret warehouse, where we assume to find all the stolen stuff. The door to 
-this warehouse is secured by an electronic combination lock. Unfortunately our spy isn't sure about the 
+Alright, detective, one of our colleagues successfully observed our target person, Robby the robber.
+We followed him to a secret warehouse, where we assume to find all the stolen stuff. The door to
+this warehouse is secured by an electronic combination lock. Unfortunately our spy isn't sure about the
 PIN he saw, when Robby entered it.
 
 The keypad has the following layout:
@@ -15,18 +15,18 @@ The keypad has the following layout:
 └───┼───┼───┘
     │ 0 │
     └───┘
-He noted the PIN 1357, but he also said, it is possible that each of the digits he saw could actually 
-be another adjacent digit (horizontally or vertically, but not diagonally). E.g. instead of the 1 it 
+He noted the PIN 1357, but he also said, it is possible that each of the digits he saw could actually
+be another adjacent digit (horizontally or vertically, but not diagonally). E.g. instead of the 1 it
 could also be the 2 or 4. And instead of the 5 it could also be the 2, 4, 6 or 8.
 
-He also mentioned, he knows this kind of locks. You can enter an unlimited amount of wrong PINs, they 
+He also mentioned, he knows this kind of locks. You can enter an unlimited amount of wrong PINs, they
 never finally lock the system or sound the alarm. That's why we can try out all possible (*) variations.
 
 * possible in sense of: the observed PIN itself and all variations considering the adjacent digits
 
-Can you help us to find all those variations? It would be nice to have a function, that returns an array 
-of all variations for an observed PIN with a length of 1 to 8 digits. We could name the function getPINs. 
-But please note that all PINs, the observed one and also the results, must be strings, because of 
+Can you help us to find all those variations? It would be nice to have a function, that returns an array
+of all variations for an observed PIN with a length of 1 to 8 digits. We could name the function getPINs.
+But please note that all PINs, the observed one and also the results, must be strings, because of
 potentially leading '0's. Don't worry about the order of the array.
 
 Detective, we count on you!
@@ -40,11 +40,34 @@ expectations = {
 */
 
 
-
+const adj = {
+  1: ['1', '2', '4'],
+  2: ['2', '1', '3', '5'],
+  3: ['3', '2', '6'],
+  4: ['4', '1', '5', '7'],
+  5: ['5', '2', '4', '6', '8'],
+  6: ['6', '9', '3', '5'],
+  7: ['7', '4', '8'],
+  8: ['8', '7', '5', '9', '0'],
+  9: ['9', '8', '6'],
+  0: ['0', '8'],
+};
 
 function getPINs(observed) {
-
+  if (!observed) return [];
+  if (observed.length === 1) return adj[observed];
+  const result = [];
+  const firstChar = observed[0];
+  const remaining = observed.slice(1);
+  const prev = getPINs(remaining);
+  adj[firstChar].forEach((el) => {
+    prev.forEach((str) => {
+      result.push(el + str);
+    });
+  });
+  return result;
 }
 
+console.log(getPINs('369'));
 
-module.exports = getPINs
+module.exports = getPINs;
