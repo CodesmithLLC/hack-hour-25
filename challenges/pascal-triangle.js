c7ff938472d9778269912b8c03ef58d67bfbd462
Dave Marquess <dave.marquess@gmail.com>
2019-01-15 09:59:20 -0800

    attempt pascal

--- challenges/pascal-triangle.js
+++ challenges/pascal-triangle.js
@@ -33,7 +33,27 @@
 */
 
 function pascalTriangle(numRows) {
-
+  // create result array
+  const resultArr = [];
+  // iterate from 1 up to numRows
+  for (let i = 1; i <= numRows; i += 1) {
+    const rowArr = [];
+    // iterate from 1 to current num
+    for (let x = 1; x <= i; x += 1) {
+      // if num is less than or equal to 2, push 1 onto array and push array onto result array
+      if (x <= 2) rowArr.push(x);
+      // otherwise, iterate from 0th el to end of result array, pushing el plus subsequen el onto new arr
+      if (x > 2) {
+        for (let j = 2; j < resultArr[j - 1].length; j += 1) {
+          rowArr.push(resultArr[j - 1][j] + resultArr[j - 1][j + 1])
+        }
+      }
+    }
+    resultArr.push(rowArr);
+  }
+  return resultArr;
 }
+
+console.log(pascalTriangle(6));
 
 module.exports = pascalTriangle;
