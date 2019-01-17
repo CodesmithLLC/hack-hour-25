5df6d1efa8d97a3d5fadc4025756fa61364d7c34
codemccodeface <yurenfrankma@gmail.com>
2019-01-17 09:31:23 -0800

    finished linked list cycle

--- challenges/linked-list-cycle.js
+++ challenges/linked-list-cycle.js
@@ -27,13 +27,28 @@
  *
  */
 
-var Node = function(value) {
+const Node = function (value) {
   this.value = value;
   this.next = null;
-}
+};
 
 function hasCycle(head) {
-
+  let curr = head;
+  const nodes = [curr];
+  while (curr.next) {
+    if (nodes.includes(curr.next)) return true;
+    nodes.push(curr);
+    curr = curr.next;
+  }
+  return false;
 }
 
-module.exports = {Node: Node, hasCycle: hasCycle}
+const node1 = new Node('1');
+const node2 = node1.next = new Node('2');
+const node3 = node2.next = new Node('3');
+const node4 = node3.next = new Node('4');
+const node5 = node4.next = new Node('5');
+console.log(hasCycle(node1)); // => false
+node5.next = node2;
+console.log(hasCycle(node1)); // => true
+module.exports = { Node, hasCycle };
