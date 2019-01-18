8c194f4cad9c7c80ee1739543e9ee601be05e7ce
codemccodeface <yurenfrankma@gmail.com>
2019-01-18 09:34:10 -0800

    finished knapsack

--- challenges/knapsack-problem.js
+++ challenges/knapsack-problem.js
@@ -9,8 +9,24 @@
   solveKnapsack(items, 5); // returns 9 (from items[1] and items[2])
 */
 
-function solveKnapsack(items, weightAvailable) {
+function solveKnapsack(items, weightAvailable, currVal = 0) {
+  if (items.length === 0) return currVal;
+  const firstItemWeight = items[0].weight;
+  const firstItemVal = items[0].value;
+  const maxWeightSkipFirst = solveKnapsack(items.slice(1), weightAvailable, currVal);
+  if (weightAvailable - firstItemWeight >= 0) {
+    // if can use first
+    const maxWeightUseFirst = solveKnapsack(
+      items.slice(1),
+      weightAvailable - firstItemWeight,
+      currVal + firstItemVal,
+    );
+    return Math.max(maxWeightSkipFirst, maxWeightUseFirst);
+  }
+  return maxWeightSkipFirst;
+}
 
-};
-
+const items = [{ weight: 1, value: 3 }, { weight: 2, value: 4 }, { weight: 3, value: 5 }];
+console.log(solveKnapsack(items, 3)); // returns 7 (from items[0] and items[1])
+console.log(solveKnapsack(items, 5));
 module.exports = solveKnapsack;
