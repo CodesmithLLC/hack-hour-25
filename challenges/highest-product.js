66fc28a30bc4c53f5742e1f34182fdea90bf7974
vwang4536 <vwang4536@gmail.com>
2019-01-22 09:30:17 -0800

    complete highestproduct

--- challenges/highest-product.js
+++ challenges/highest-product.js
@@ -6,11 +6,11 @@ function highestProduct(array) {
 //triple nested iteration
     let check;
     for (let i = 0; i < array.length-2; i += 1) {
-        for (let j = 1; j < array.length-1; j += 1) {
-            for (let k = 2; k < array.length; k += 1) {
+        for (let j = i + 1; j < array.length-1; j += 1) {
+            for (let k = j + 1; k < array.length; k += 1) {
                 let sum;
                 sum = array[i] * array[j] * array[k];
-                console.log(array[i], array[k], array[j])
+                console.log(array[i], array[k], array[j], sum)
                 if (!check || sum > check) {
                     check = sum;
                 }
@@ -20,7 +20,7 @@ function highestProduct(array) {
     return check
 }
 
-console.log(highestProduct([1,2,3,4]))
+console.log(highestProduct([1,2,3,4,5,1]))
 
 
 module.exports = highestProduct;
