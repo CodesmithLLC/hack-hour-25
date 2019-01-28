86e41a08823e61f2fe5b2c51805d5b4d14554d60
codemccodeface <yurenfrankma@gmail.com>
2019-01-28 10:58:26 -0800

    highest-product

--- challenges/highest-product.js
+++ challenges/highest-product.js
@@ -2,9 +2,29 @@
  * Given an array of integers, find the highest product you can get from three of the integers.
  */
 
-function highestProduct(array) {
-
+// recursive
+function highestProduct(array, currProd = 1, intLeft = 3) {
+  if (intLeft === 0) return currProd;
+  if (array.length === 0) return -Infinity;
+  const newArr1 = [...array];
+  const first = newArr1.shift();
+  // console.log('first: ', first);
+  const intDecrement = intLeft - 1;
+  const useFirst = highestProduct(newArr1, currProd * first, intDecrement);
+  const dontUseFirst = highestProduct(newArr1, currProd, intLeft);
+  return Math.max(useFirst, dontUseFirst);
 }
 
+// recursive
+function highestProduct2(array) {
+  // get array of positives
+  // arro of zeroes
+  // arr of negatives
+  // if two or more negatives, see if two negatives product bigger than
+  const negatives = [];
+  const zeroes = [];
+}
+
+console.log(highestProduct([2, 3, 4, 5, 6]));
 
 module.exports = highestProduct;
