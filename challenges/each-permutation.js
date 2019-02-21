3be8b5cf29ffef67d75600ec0e387db7f8ee20da
Jason Wong <jwaosnogn@gmail.com>
2019-02-21 15:51:01 -0800

    permutation

--- challenges/each-permutation.js
+++ challenges/each-permutation.js
@@ -21,9 +21,25 @@ eachPermutation([1, 2, 3], function(perm) {
 */
 
 function eachPermutation(arr, callback) {
+  function swap(array, i, j) {
+    const temp = array[i];
+    array[i] = array[j];
+    array[j] = temp;
+  }
 
+  function heaps(i) {
+    if (i === arr.length) {
+      callback(arr);
+    }
+    for (let j = i; j < arr.length; j += 1) {
+      swap(arr, i, j);
+      heaps(i + 1);
+      swap(arr, i, j);
+    }
+  }
+
+  heaps(0);
 }
-
 
 
 module.exports = eachPermutation;
