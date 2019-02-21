8cb91f383c6034cfd8abeb65809ccaa73615a5bf
Donte' Nall <code4zelda@gmail.com>
2019-02-20 23:25:49 -0800

    update

--- challenges/stack-queue.js
+++ challenges/stack-queue.js
@@ -8,23 +8,36 @@
 // CHECK IF S2 IS FILLED 
 // IF SO  
 function Stack() {
-    // build array 
     this.items = [];
-    this.length = 0;
-
-    Stack.prototype.push()
-
-}
-
-
-/**
-* Queue Class
-*/
-
-
-function Queue() {
-
-}
-
-
+  }
+  
+  Stack.prototype.push = function(item) {
+    this.items.push(item);
+  };
+  
+  Stack.prototype.pop = function() {
+    return this.items.pop();
+  };
+  
+  Stack.prototype.isEmpty = function() {
+    return this.items.length;
+  };
+  
+  function Queue() {
+    this.in = new Stack();
+    this.out = new Stack();
+  }
+  
+  Queue.prototype.enqueue = function(item) {
+    this.in.push(item);
+  };
+  
+  Queue.prototype.dequeue = function() {
+    if (this.out.isEmpty()) {
+      while (!this.in.isEmpty()) {
+        this.out.push(this.in.pop());
+      }
+    }
+    return this.out.pop();
+  };
 module.exports = { Stack: Stack, Queue: Queue };
